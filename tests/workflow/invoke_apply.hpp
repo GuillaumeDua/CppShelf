8de6d44e6122d804b9eb01a0481eb1696c12f497
GuillaumeDua <guillaume.dua@gmail.com>
2021-09-08 19:47:18 +0200

    [cpp/workflow] apply : tests

--- tests/workflow/invoke_apply.hpp
+++ tests/workflow/invoke_apply.hpp
@@ -75,74 +75,103 @@ namespace test::functional::invocation {
         }
     }
     consteval void applyable() {
-        using namespace workflow::functional;
+        namespace ns = workflow::functional; // stl namespace clash https://godbolt.org/z/Kz9PjMs3P
 
         // f()
         {
             constexpr auto f = [](){};
 
-            static_assert(requires{ apply(f, std::tuple{}); });
-            static_assert(requires{ apply<>(f, std::tuple{}); });
-
-            static_assert(not noexcept(invoke(f)));
-            static_assert(not noexcept(invoke<>(f)));
+            static_assert(requires{ ns::apply(f, std::tuple{}); });
+            static_assert(requires{ ns::apply<>(f, std::tuple{}); });
+            static_assert(not noexcept(ns::apply(f, std::tuple{})));
+            static_assert(not noexcept(ns::apply<>(f, std::tuple{})));
         }
         {
             constexpr auto f = []() noexcept {};
 
-            static_assert(requires{ invoke(f); });
-            static_assert(requires{ invoke<>(f); });
-
-            static_assert(noexcept(invoke(f)));
-            static_assert(noexcept(invoke<>(f)));
+            static_assert(requires{ ns::apply(f, std::tuple{}); });
+            static_assert(requires{ ns::apply<>(f, std::tuple{}); });
+            static_assert(noexcept(ns::apply(f, std::tuple{})));
+            static_assert(noexcept(ns::apply<>(f, std::tuple{})));
         }
-        // f(...)
         {
             constexpr auto f = [](int, bool){};
 
-            static_assert(requires{ invoke(f, 42, true); });
-            static_assert(requires{ invoke<>(f, 42, true); });
-
-            static_assert(not noexcept(invoke(f, 42, true)));
-            static_assert(not noexcept(invoke<>(f, 42, true)));
+            static_assert(requires{ ns::apply(f, std::tuple{42, true}); });
+            static_assert(requires{ ns::apply<>(f, std::tuple{42, true}); });
+            static_assert(not noexcept(ns::apply(f, std::tuple{42, true})));
+            static_assert(not noexcept(ns::apply<>(f, std::tuple{42, true})));
         }
         {
             constexpr auto f = [](int, bool) noexcept {};
 
-            static_assert(requires{ invoke(f, 42, true); });
-            static_assert(requires{ invoke<>(f, 42, true); });
-
-            static_assert(noexcept(invoke(f, 42, true)));
-            static_assert(noexcept(invoke<>(f, 42, true)));
+            static_assert(requires{ ns::apply(f, std::tuple{42, true}); });
+            static_assert(requires{ ns::apply<>(f, std::tuple{42, true}); });
+            static_assert(noexcept(ns::apply(f, std::tuple{42, true})));
+            static_assert(noexcept(ns::apply<>(f, std::tuple{42, true})));
         }
         // f<...>()
         {
             constexpr auto f = []<typename, typename>(){};
 
-            static_assert(requires{ invoke<int, char>(f); });
-            static_assert(not noexcept(invoke<int, char>(f)));
+            static_assert(requires{ ns::apply<char, int>(f, std::tuple{}); });
+            static_assert(not noexcept(ns::apply<char, int>(f, std::tuple{})));
+        }
+        {
+            constexpr auto f = []<typename, typename>() noexcept {};
+
+            static_assert(requires{ ns::apply<char, int>(f, std::tuple{}); });
+            static_assert(noexcept(ns::apply<char, int>(f, std::tuple{})));
         }
         // f<T>(T)
         {
             constexpr auto f = []<typename T>(T){};
 
-            static_assert(requires{ invoke(f, 42); });
-            static_assert(requires{ invoke<>(f, 42); });
-            static_assert(requires{ invoke<int>(f, 42); });
+            static_assert(requires{ ns::apply(f, std::tuple{'a'}); });
+            static_assert(requires{ ns::apply<>(f, std::tuple{'a'}); });
+            static_assert(requires{ ns::apply<char>(f, std::tuple{'a'}); });
+            
+            static_assert(not noexcept(ns::apply(f, std::tuple{'a'})));
+            static_assert(not noexcept(ns::apply<>(f, std::tuple{'a'})));
+            static_assert(not noexcept(ns::apply<char>(f, std::tuple{'a'})));
+        }
+        {
+            constexpr auto f = []<typename T>(T) noexcept {};
 
-            static_assert(not noexcept(invoke(f, 42)));
-            static_assert(not noexcept(invoke<>(f, 42)));
-            static_assert(not noexcept(invoke<int>(f, 42)));
+            static_assert(requires{ ns::apply(f, std::tuple{'a'}); });
+            static_assert(requires{ ns::apply<>(f, std::tuple{'a'}); });
+            static_assert(requires{ ns::apply<char>(f, std::tuple{'a'}); });
+            
+            static_assert(noexcept(ns::apply(f, std::tuple{'a'})));
+            static_assert(noexcept(ns::apply<>(f, std::tuple{'a'})));
+            static_assert(noexcept(ns::apply<char>(f, std::tuple{'a'})));
         }
         // f<t...>(auto...)
         {
             constexpr auto f = []<typename...>(auto...){};
 
-            static_assert(requires{ invoke(f, 42); });
-            static_assert(requires{ invoke<>(f, 42); });
-            static_assert(requires{ invoke<int>(f, 42); });
-            static_assert(requires{ invoke<int, char>(f, 42); });
-            static_assert(requires{ invoke<int, char>(f, 42, true, 'a'); });
+            static_assert(requires{ ns::apply(f, std::tuple{'a'}); });
+            static_assert(requires{ ns::apply<>(f, std::tuple{'a'}); });
+            static_assert(requires{ ns::apply<char>(f, std::tuple{'a'}); });
+            static_assert(requires{ ns::apply<int, char>(f, std::tuple{'a'}); });
+            
+            static_assert(not noexcept(ns::apply(f, std::tuple{'a'})));
+            static_assert(not noexcept(ns::apply<>(f, std::tuple{'a'})));
+            static_assert(not noexcept(ns::apply<char>(f, std::tuple{'a'})));
+            static_assert(not noexcept(ns::apply<int, char>(f, std::tuple{'a'})));
+        }
+        {
+            constexpr auto f = []<typename...>(auto...) noexcept {};
+
+            static_assert(requires{ ns::apply(f, std::tuple{'a'}); });
+            static_assert(requires{ ns::apply<>(f, std::tuple{'a'}); });
+            static_assert(requires{ ns::apply<char>(f, std::tuple{'a'}); });
+            static_assert(requires{ ns::apply<int, char>(f, std::tuple{'a'}); });
+            
+            static_assert(noexcept(ns::apply(f, std::tuple{'a'})));
+            static_assert(noexcept(ns::apply<>(f, std::tuple{'a'})));
+            static_assert(noexcept(ns::apply<char>(f, std::tuple{'a'})));
+            static_assert(noexcept(ns::apply<int, char>(f, std::tuple{'a'})));
         }
     }
 }
