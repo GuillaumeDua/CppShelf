5eee4c321905930dc221020580aeca57821a2e23
GuillaumeDua <guillaume.dua@gmail.com>
2021-09-30 18:38:20 +0200

    [csl::mp] add type/pack traits

--- mp/mp.hpp
+++ mp/mp.hpp
@@ -4,6 +4,7 @@
 #define static_dependent_error(message) static_assert([](){ return false; }(), message)
 
 // cpp shelf library : metaprogramming
+// sequences
 namespace csl::mp::seq {
     // reverse_integer_sequence
     template <typename T, T ... values>
@@ -54,6 +55,187 @@ namespace csl::mp::seq {
     constexpr decltype(auto) get(std::integer_sequence<T, values...>) noexcept {
         return get<index, values...>;
     }
+}
+
+// abstraction on (ttps...|pack<ttps...>)
+namespace csl::mp {
+
+    template <typename T>
+    concept TupleType = requires { std::tuple_size_v<T>; };
+
+    // get<index>
+    template <std::size_t index, typename ... Ts>
+    constexpr decltype(auto) get(Ts && ... values) noexcept {
+        return std::get<index>(std::tuple<decltype(values)...>{ fwd(values)...});
+    }
+    template <std::size_t index, TupleType T>
+    constexpr decltype(auto) get(T && value) noexcept {
+        return std::get<index>(value);
+    }
+    template <std::size_t index, typename T, auto ... values>
+    constexpr decltype(auto) get(std::integer_sequence<T, values...> && value) noexcept {
+        return mp::seq::get<index>(fwd(value));
+    }
+
+    #pragma region is_template
+    // is_template : ttps
+    template <typename ...>
+    struct is_template_with_ttps : std::false_type{};
+    template <template <typename...> typename pack_type, typename ... Ts>
+    struct is_template_with_ttps<pack_type<Ts...>> : std::true_type{};
+    template <typename ... Ts>
+    constexpr bool is_template_with_ttps_v = is_template_with_ttps<Ts...>::value;
+
+    template <typename T>
+    concept TemplateType_ttps = is_template_with_ttps_v<T>;
+
+    // is_template : nttps
+    template <typename ...>
+    struct is_template_with_nttps : std::false_type{};
+    template <template <auto...> typename pack_type, auto ... values>
+    struct is_template_with_nttps<pack_type<values...>> : std::true_type{};
+    template <typename ... Ts>
+    constexpr bool is_template_with_nttps_v = is_template_with_nttps<Ts...>::value;
+
+    template <typename T>
+    concept TemplateType_nttps = is_template_with_nttps_v<T>;
+
+    // is_template
+    template <typename ... Ts>
+    struct is_template {
+        constexpr static bool value = is_template_with_ttps_v<Ts...> or is_template_with_nttps_v<Ts...>;
+    };
+    template <typename ... Ts>
+    constexpr bool is_template_v = is_template<Ts...>::value;
+
+    template <typename T>
+    concept TemplateType = is_template_v<T>;
+
+    #pragma endregion
+
+    // contains
+    template <typename T, typename ... ttps>
+    struct contains : std::bool_constant<(std::is_same_v<T, ttps> or ...)>{};
+    template <typename T, template <typename...> typename pack, typename ... ttps>
+    struct contains<T, pack<ttps...>> : contains<T, ttps...>{};
+    template <typename T, typename ... ttps>
+    constexpr bool contains_v = contains<T, ttps...>::value;
+
+    // count
+    template <typename T, typename ... ttps>
+    struct count : std::integral_constant<std::size_t, (std::size_t{std::is_same_v<T, ttps>} + ...)>{};
+    template <typename T, template <typename...> typename pack, typename ... ttps>
+    struct count<T, pack<ttps...>> : count<T, ttps...>{};
+    template <typename T, typename ... ttps>
+    constexpr std::size_t count_v = count<T, ttps...>::value;
+
+    // append
+    template <typename, typename...>
+    struct append;
+    template <
+        template <typename...> typename pack,
+        typename ... Ts,
+        typename ... Us
+    >
+    struct append<pack<Ts...>, Us...> : std::type_identity<pack<Ts..., Us...>>{};
+    template <
+        template <typename...> typename pack,
+        typename ... Ts,
+        typename ... Us
+    >
+    struct append<pack<Ts...>, pack<Us...>> : std::type_identity<pack<Ts..., Us...>>{};
+    template <typename pack, typename... ttps>
+    using append_t = append<pack, ttps...>;
+
+    // merge
+    //  todo : add if not duplicates ?
+
+    // repack_into
+    template <template <typename...> typename destination, typename ... Ts>
+    struct unfold_to : std::type_identity<destination<Ts...>>{};
+    template <template <typename...> typename destination, template <typename...> typename from, typename ... Ts>
+    struct unfold_to<destination, from<Ts...>> : unfold_to<destination, Ts...>{};
+    template <template <typename...> typename destination, typename ... from>
+    using unfold_to_t = unfold_to<destination, from...>::type;
+
+    // reverse
+    template <typename>
+    struct reverse;
+    template <template <typename ...> typename pack_type, typename ... Ts>
+    class reverse<pack_type<Ts...>> {
+        constexpr static auto impl() {
+            return [](){
+                // todo : reverse_sequence, type_at
+            }();
+        }
+    };
+    template <typename T>
+    using reverse_t = reverse<T>::type;
+
+    // rindex_of
+    template <typename T, typename ... Ts>
+    struct rindex_of {
+        constexpr static std::size_t value = rindex_of<T, unfold_to_t<std::tuple, Ts...>>::value;
+    };
+    template <typename T, template <typename ...> typename tuple_type>
+    struct rindex_of<T, tuple_type<>> {
+        static_assert([](){ return false; }(), "(r)index_of : not found");
+    };
+    template <typename T, template <typename ...> typename tuple_type, typename ... rest>
+    struct rindex_of<T, tuple_type<T, rest...>> {
+        constexpr static std::size_t value = sizeof...(rest);
+    };
+    template <typename T, template <typename ...> typename tuple_type, typename first, typename ... rest>
+    struct rindex_of<T, tuple_type<first, rest...>> {
+        constexpr static std::size_t value = rindex_of<T, tuple_type<rest...>>::value;
+    };
+    template <typename T, typename ... Ts>
+    constexpr std::size_t rindex_of_v = rindex_of<T, Ts...>::value;
+
+    // index_of
+    template <typename T, typename ... Ts>
+    struct index_of {
+        constexpr static std::size_t value = sizeof...(Ts) - rindex_of_v<T, Ts...> - 1;
+    };
+    template <typename T, template <typename ...> typename tuple_type, typename ... Ts>
+    struct index_of<T, tuple_type<Ts...>> {
+        constexpr static std::size_t value = sizeof...(Ts) - rindex_of_v<T, tuple_type<Ts...>> - 1;
+    };
+    template <typename T, typename ... Ts>
+    constexpr std::size_t index_of_v = index_of<T, Ts...>::value;
+
+    // at
+
+    // deduplicate
+    template <typename ...>
+    struct deduplicate_impl;
+    template <typename T>
+    struct deduplicate_impl<T> {
+        using type = std::tuple<T>;
+    };
+    template <typename first, typename ... rest>
+    struct deduplicate_impl<first, rest...> {
+        using type = decltype(std::tuple_cat(
+            std::conditional_t<
+                contains_v<first, std::tuple<rest...>>,
+                std::tuple<>,
+                std::tuple<first>
+            >{},
+            typename deduplicate_impl<rest...>::type{}
+        ));
+    };
+
+    template <typename T>
+    struct deduplicate;
+    template <template <typename...> typename type_pack, typename ... Ts>
+    class deduplicate<type_pack<Ts...>> {
+
+        using tuple_type = typename deduplicate_impl<Ts...>::type;
+    public:
+        using type = unfold_to_t<type_pack, tuple_type>;
+    };
+    template <typename T>
+    using deduplicate_t = deduplicate<T>::type;
 }
 
 #undef fwd
