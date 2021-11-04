5022c5214be6876daa6d727121f22ff07899fa2e
GuillaumeDua <guillaume.dua@gmail.com>
2021-11-04 23:49:23 +0100

    [csl::wf] route : tests : invocable, invoke_result_t (fix)

--- tests/workflow/route.hpp
+++ tests/workflow/route.hpp
@@ -7,31 +7,36 @@ namespace test::route_ {
     auto node_2 = [](int i){ return i + 1; };
     auto node_3 = [](){};
 
+    # pragma region fwd (node_1 >[int]> node_2)
     // std::invocable
     static_assert(std::invocable<
-        decltype(csl::wf::route{ node_1 }),
-        int
+        decltype(csl::wf::route{ node_1 })
     >);
     static_assert(std::invocable<
-        decltype(csl::wf::route{ node_1, node_2 }),
-        int
-    >);
-    static_assert(std::invocable<
-        decltype(csl::wf::route{ node_1, node_2, node_3 }),
-        int
+        decltype(csl::wf::route{ node_1, node_2 })
     >);
 
     // invoke_result_t
     static_assert(std::same_as<
-        invoke_result_t<decltype(csl::wf::route{ node_1 })>,
+        std::invoke_result_t<decltype(csl::wf::route{ node_1 })>,
         int
     >);
-    static_assert(std::invocable<
-        invoke_result_t<decltype(csl::wf::route{ node_1, node_2 })>,
+    static_assert(std::same_as<
+        std::invoke_result_t<decltype(csl::wf::route{ node_1, node_2 })>,
         int
     >);
-    static_assert(std::invocable<
-        invoke_result_t<decltype(csl::wf::route{ node_1, node_2, node_3 })>,
-        int
-    >);
+    #pragma endregion
+
+    # pragma region fwd, discard (node_1 >[int]> node_2 >> node_3)
+    // requires discard feature
+    //
+    // static_assert(std::invocable<
+    //     decltype(csl::wf::route{ node_1, node_2, node_3 }),
+    //     int
+    // >);
+    // static_assert(std::invocable<
+    //     invoke_result_t<decltype(csl::wf::route{ node_1, node_2, node_3 })>,
+    //     int
+    // >);
+    #pragma endregion
 }
