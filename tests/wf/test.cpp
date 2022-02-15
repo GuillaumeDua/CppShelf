692279fa016bc55e50d24359d901991a9571aadb
GuillaumeDua <guillaume.dua@gmail.com>
2022-02-15 23:45:43 +0100

    [project] wf : re-enable route (tests)

--- tests/wf/test.cpp
+++ tests/wf/test.cpp
@@ -69,60 +69,60 @@ auto main() -> int {
             >
         );
 
-        // auto route = csl::wf::route {
-        //     lvalue_node_type{},
-        //     lvalue_node_type{},
-        //     lvalue_node_type{},
-        //     lvalue_node_type{}
-        // };
-        // route();
+        auto route = csl::wf::route {
+            lvalue_node_type{},
+            lvalue_node_type{},
+            lvalue_node_type{},
+            lvalue_node_type{}
+        };
+        route();
     }
-    // {
-    //     auto node_1 = [](){ return 42; };
-    //     auto node_2 = [](int){};
-    //     auto node_3 = [](){};
+    {
+        auto node_1 = [](){ return 42; };
+        auto node_2 = [](int){};
+        auto node_3 = [](){};
 
-    //     static_assert(csl::wf::mp::chain_trait<
-    //         decltype(node_2), decltype(node_3)
-    //     >::is_invocable<int>);
-    //     static_assert(csl::wf::mp::chain_trait<
-    //         decltype(node_1), decltype(node_2), decltype(node_3)
-    //     >::is_invocable<>);
+        static_assert(csl::wf::mp::chain_trait<
+            decltype(node_2), decltype(node_3)
+        >::is_invocable<int>);
+        static_assert(csl::wf::mp::chain_trait<
+            decltype(node_1), decltype(node_2), decltype(node_3)
+        >::is_invocable<>);
 
-    //     csl::wf::chain_invoke(std::tuple{
-    //         node_1, node_2, node_3
-    //     }, std::tuple{});
-    // }
-    // {   // fwd arg
-    //     auto route = csl::wf::route {
-    //         [](){ return 42; },
-    //         [](int){  },
-    //         [](){}
-    //     };
-    //     route();
-    // }
+        csl::wf::chain_invoke(std::tuple{
+            node_1, node_2, node_3
+        }, std::tuple{});
+    }
+    {   // fwd arg
+        auto route = csl::wf::route {
+            [](){ return 42; },
+            [](int){  },
+            [](){}
+        };
+        route();
+    }
 
-    // using namespace csl::wf::operators;
+    using namespace csl::wf::operators;
 
-    // auto route = [](){
-    //     std::cout << "A";
-    // } >>=
-    // [](){
-    //     std::cout << "B";
-    // } >>=
-    // [](){
-    //     std::cout << "C\n";
-    // };
-    // route();
-    // std::move(route)();
-    // std::as_const(route)();
-    // std::move(std::as_const(route))();
+    auto route = [](){
+        std::cout << "A";
+    } >>=
+    [](){
+        std::cout << "B";
+    } >>=
+    [](){
+        std::cout << "C\n";
+    };
+    route();
+    std::move(route)();
+    std::as_const(route)();
+    std::move(std::as_const(route))();
 
-    // std::cout << gcl::cx::type_name_v<decltype(route)> << '\n';
+    std::cout << gcl::cx::type_name_v<decltype(route)> << '\n';
 
-    // auto route_2 = std::move(route) >>= [](){
-    // // auto route_2 = route >>= [](){
-    //     std::cout << "D\n";
-    // };
-    // std::cout << gcl::cx::type_name_v<decltype(route_2)> << '\n';
+    auto route_2 = std::move(route) >>= [](){
+    // auto route_2 = route >>= [](){
+        std::cout << "D\n";
+    };
+    std::cout << gcl::cx::type_name_v<decltype(route_2)> << '\n';
 }
