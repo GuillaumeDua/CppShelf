a45e8c57066eb60267801e33334da3422346d2b0
GuillaumeDua <guillaume.dua@gmail.com>
2022-04-28 23:05:03 +0200

    [project] wf : #37 bind_back : bind_back : (tests) first back_binder, then bind_back

--- tests/wf/bind_back.hpp
+++ tests/wf/bind_back.hpp
@@ -4,57 +4,6 @@
 #include <memory>
 #include <utils/semantic_types.hpp>
 
-namespace test {
-    consteval void bind_back_() {
-        auto func = []<typename ... Ts>() noexcept { return std::tuple<Ts...>{}; };
-
-        using namespace csl::wf;
-
-        // operator()
-        static_assert(std::is_same_v<decltype(bind_back(func)()), std::tuple<>>);
-        static_assert(std::is_same_v<decltype(bind_back<int>(func)()), std::tuple<int>>);
-        static_assert(std::is_same_v<decltype(bind_back<int, char>(func)()), std::tuple<int, char>>);
-        static_assert(std::is_same_v<decltype(bind_back<int>(func).template operator()<char>()), std::tuple<int, char>>);
-        static_assert(std::is_same_v<decltype(bind_back(func).template operator()<int, char>()), std::tuple<int, char>>);
-
-        namespace mp = csl::wf::mp;
-
-        // is_invocable
-        static_assert(mp::is_invocable_v<decltype(bind_back(func))>);
-        static_assert(mp::is_invocable_v<decltype(bind_back<int>(func))>);
-        static_assert(mp::is_invocable_v<decltype(bind_back<int, char>(func))>);
-        static_assert(mp::is_invocable_v<decltype(bind_back<int>(func)), mp::ttps<char>>);
-        static_assert(mp::is_invocable_v<decltype(bind_back(func)), mp::ttps<int, char>>);
-
-        // is_nothrow_invocable
-        static_assert(mp::is_nothrow_invocable_v<decltype(bind_back(func))>);
-        static_assert(mp::is_nothrow_invocable_v<decltype(bind_back<int>(func))>);
-        static_assert(mp::is_nothrow_invocable_v<decltype(bind_back<int, char>(func))>);
-        static_assert(mp::is_nothrow_invocable_v<decltype(bind_back<int>(func)), mp::ttps<char>>);
-        static_assert(mp::is_nothrow_invocable_v<decltype(bind_back(func)), mp::ttps<int, char>>);
-
-        // is_invocable_r_v
-        static_assert(mp::is_invocable_r_v<std::tuple<>,             decltype(bind_back(func))>);
-        static_assert(mp::is_invocable_r_v<std::tuple<int>,          decltype(bind_back<int>(func))>);
-        static_assert(mp::is_invocable_r_v<std::tuple<int, char>,    decltype(bind_back<int, char>(func))>);
-        static_assert(mp::is_invocable_r_v<std::tuple<int, char>,    decltype(bind_back<int>(func)), mp::ttps<char>>);
-        static_assert(mp::is_invocable_r_v<std::tuple<int, char>,    decltype(bind_back(func)), mp::ttps<int, char>>);
-
-        // is_nothrow_invocable_r_v
-        static_assert(mp::is_nothrow_invocable_r_v<std::tuple<>,             decltype(bind_back(func))>);
-        static_assert(mp::is_nothrow_invocable_r_v<std::tuple<int>,          decltype(bind_back<int>(func))>);
-        static_assert(mp::is_nothrow_invocable_r_v<std::tuple<int, char>,    decltype(bind_back<int, char>(func))>);
-        static_assert(mp::is_nothrow_invocable_r_v<std::tuple<int, char>,    decltype(bind_back<int>(func)), mp::ttps<char>>);
-        static_assert(mp::is_nothrow_invocable_r_v<std::tuple<int, char>,    decltype(bind_back(func)), mp::ttps<int, char>>);
-
-        // same_as<return_type, r>
-        static_assert(std::same_as<std::tuple<>,             mp::invoke_result_t<decltype(bind_back(func))>>);
-        static_assert(std::same_as<std::tuple<int>,          mp::invoke_result_t<decltype(bind_back<int>(func))>>);
-        static_assert(std::same_as<std::tuple<int, char>,    mp::invoke_result_t<decltype(bind_back<int, char>(func))>>);
-        static_assert(std::same_as<std::tuple<int, char>,    mp::invoke_result_t<decltype(bind_back<int>(func)), mp::ttps<char>>>);
-        static_assert(std::same_as<std::tuple<int, char>,    mp::invoke_result_t<decltype(bind_back(func)), mp::ttps<int, char>>>);
-    }
-}
 namespace test::back_binder_ {
 
     constexpr auto func = []<typename T, typename U>(int args_0, char args_1) constexpr {
@@ -260,5 +209,56 @@ namespace test::back_binder_ {
             constexpr auto value = back_binder{ func, mp::ttps<void, void>{}, 42 };
             static_assert(not std::equality_comparable<decltype(value)>);
         }
+    }
+}
+namespace test {
+    consteval void bind_back_() {
+        auto func = []<typename ... Ts>() noexcept { return std::tuple<Ts...>{}; };
+
+        using namespace csl::wf;
+
+        // operator()
+        static_assert(std::is_same_v<decltype(bind_back(func)()), std::tuple<>>);
+        static_assert(std::is_same_v<decltype(bind_back<int>(func)()), std::tuple<int>>);
+        static_assert(std::is_same_v<decltype(bind_back<int, char>(func)()), std::tuple<int, char>>);
+        static_assert(std::is_same_v<decltype(bind_back<int>(func).template operator()<char>()), std::tuple<int, char>>);
+        static_assert(std::is_same_v<decltype(bind_back(func).template operator()<int, char>()), std::tuple<int, char>>);
+
+        namespace mp = csl::wf::mp;
+
+        // is_invocable
+        static_assert(mp::is_invocable_v<decltype(bind_back(func))>);
+        static_assert(mp::is_invocable_v<decltype(bind_back<int>(func))>);
+        static_assert(mp::is_invocable_v<decltype(bind_back<int, char>(func))>);
+        static_assert(mp::is_invocable_v<decltype(bind_back<int>(func)), mp::ttps<char>>);
+        static_assert(mp::is_invocable_v<decltype(bind_back(func)), mp::ttps<int, char>>);
+
+        // is_nothrow_invocable
+        static_assert(mp::is_nothrow_invocable_v<decltype(bind_back(func))>);
+        static_assert(mp::is_nothrow_invocable_v<decltype(bind_back<int>(func))>);
+        static_assert(mp::is_nothrow_invocable_v<decltype(bind_back<int, char>(func))>);
+        static_assert(mp::is_nothrow_invocable_v<decltype(bind_back<int>(func)), mp::ttps<char>>);
+        static_assert(mp::is_nothrow_invocable_v<decltype(bind_back(func)), mp::ttps<int, char>>);
+
+        // is_invocable_r_v
+        static_assert(mp::is_invocable_r_v<std::tuple<>,             decltype(bind_back(func))>);
+        static_assert(mp::is_invocable_r_v<std::tuple<int>,          decltype(bind_back<int>(func))>);
+        static_assert(mp::is_invocable_r_v<std::tuple<int, char>,    decltype(bind_back<int, char>(func))>);
+        static_assert(mp::is_invocable_r_v<std::tuple<int, char>,    decltype(bind_back<int>(func)), mp::ttps<char>>);
+        static_assert(mp::is_invocable_r_v<std::tuple<int, char>,    decltype(bind_back(func)), mp::ttps<int, char>>);
+
+        // is_nothrow_invocable_r_v
+        static_assert(mp::is_nothrow_invocable_r_v<std::tuple<>,             decltype(bind_back(func))>);
+        static_assert(mp::is_nothrow_invocable_r_v<std::tuple<int>,          decltype(bind_back<int>(func))>);
+        static_assert(mp::is_nothrow_invocable_r_v<std::tuple<int, char>,    decltype(bind_back<int, char>(func))>);
+        static_assert(mp::is_nothrow_invocable_r_v<std::tuple<int, char>,    decltype(bind_back<int>(func)), mp::ttps<char>>);
+        static_assert(mp::is_nothrow_invocable_r_v<std::tuple<int, char>,    decltype(bind_back(func)), mp::ttps<int, char>>);
+
+        // same_as<return_type, r>
+        static_assert(std::same_as<std::tuple<>,             mp::invoke_result_t<decltype(bind_back(func))>>);
+        static_assert(std::same_as<std::tuple<int>,          mp::invoke_result_t<decltype(bind_back<int>(func))>>);
+        static_assert(std::same_as<std::tuple<int, char>,    mp::invoke_result_t<decltype(bind_back<int, char>(func))>>);
+        static_assert(std::same_as<std::tuple<int, char>,    mp::invoke_result_t<decltype(bind_back<int>(func)), mp::ttps<char>>>);
+        static_assert(std::same_as<std::tuple<int, char>,    mp::invoke_result_t<decltype(bind_back(func)), mp::ttps<int, char>>>);
     }
 }
