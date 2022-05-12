1d003a826e3f11a3e94682ad769226624cc5ca99
GuillaumeDua <guillaume.dua@gmail.com>
2022-05-12 22:50:05 +0200

    [srl] initiale setup

---
+++ tests/srl/test.cpp
@@ -0,0 +1,11 @@
+#include <csl/srl.hpp>
+#include <iostream>
+#include <cassert>
+
+// temporarly, runtime quick-test are written here
+// until #49 is complete
+
+auto main() -> int {
+
+    std::cout << "ok\n";
+}
