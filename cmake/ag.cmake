5b02c57e0ce6dc068430ee66e0a013f4808645c4
GuillaumeDua <guillaume.dua@gmail.com>
2022-05-18 19:06:20 +0200

    [ag] cmake generates as_tuple_impl specializations

--- cmake/ag.cmake
+++ cmake/ag.cmake
@@ -12,3 +12,27 @@ if (NOT AG_MAX_FIELDS_COUNT)
 	set(AG_MAX_FIELDS_COUNT 128)
 endif()
 message(STATUS "[${CMAKE_PROJECT_NAME}] : csl::${component_name} : AG_MAX_FIELDS_COUNT set to ${AG_MAX_FIELDS_COUNT}")
+
+# Create original file
+set(ag_as_tuple_impl_specialization_filepath ${PROJECT_SOURCE_DIR}/build/generated/ag_as_tuple_impl_specialization.hpp)
+file(WRITE
+    ${ag_as_tuple_impl_specialization_filepath}
+    "// GENERATED FILE, DO NOT EDIT MANUALLY !\n\n\n"
+)
+
+set(IDENTITY_LIST "i0")
+foreach (ID RANGE 0 ${AG_MAX_FIELDS_COUNT})
+
+    if (NOT ${ID} EQUAL 0)
+        set(IDENTITY_LIST "${IDENTITY_LIST}, i${ID}")
+    endif()
+
+    file(APPEND
+        ${ag_as_tuple_impl_specialization_filepath}
+        "template <std::size_t N> requires (N == ${ID}) \
+auto as_tuple_impl(Aggregate auto && value) {
+\tauto & [ ${IDENTITY_LIST} ] = value;
+\treturn std::tie(value);
+}\n"
+    )
+endforeach()
