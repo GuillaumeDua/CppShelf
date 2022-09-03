1b690ef91323a81737868e0a5c7d534e71fb74e4
GuillaumeDua <pro.guillaume.dua@gmail.com>
2022-09-03 23:22:12 +0000

    Deploying to gh-pages from @ GuillaumeDua/CppShelf@467804757cec44479786e574abb682b29896c51f 🚀

--- output/ag/navtreedata.js
+++ output/ag/navtreedata.js
@@ -24,45 +24,46 @@ var NAVTREE =
 [
   [ "📦 csl::ag", "index.html", [
     [ "Overall presentation", "index.html", [
-      [ "Introduction", "index.html#autotoc_md1", null ],
-      [ "Philosophy & design choices", "index.html#autotoc_md4", null ],
-      [ "Getting starting", "index.html#autotoc_md6", [
-        [ "Simple usage", "index.html#autotoc_md7", null ],
-        [ "CMake", "index.html#autotoc_md8", null ],
-        [ "Configuration", "index.html#autotoc_md9", [
-          [ "<strong>Bitfields support</strong>", "index.html#autotoc_md10", null ],
-          [ "<strong>Highier limit for aggregate field count</strong>", "index.html#autotoc_md11", null ]
+      [ "Overview demo", "index.html#autotoc_md1", null ],
+      [ "Introduction", "index.html#autotoc_md2", null ],
+      [ "Philosophy & design choices", "index.html#autotoc_md5", null ],
+      [ "Getting starting", "index.html#autotoc_md7", [
+        [ "Simple usage", "index.html#autotoc_md8", null ],
+        [ "CMake", "index.html#autotoc_md9", null ],
+        [ "Configuration", "index.html#autotoc_md10", [
+          [ "<strong>Bitfields support</strong>", "index.html#autotoc_md11", null ],
+          [ "<strong>Highier limit for aggregate field count</strong>", "index.html#autotoc_md12", null ]
         ] ]
       ] ],
-      [ "Content", "index.html#autotoc_md13", [
-        [ "Aggregate-related concepts", "index.html#autotoc_md14", [
-          [ "<tt>unqualified_aggregate<T></tt>", "index.html#autotoc_md15", null ],
-          [ "<tt>aggregate<T></tt>", "index.html#autotoc_md16", null ],
-          [ "<tt>aggregate_constructible_from<T, args_ts...></tt>", "index.html#autotoc_md17", null ],
-          [ "<tt>aggregate_constructible_from_n_values<T, std::size_t N></tt>", "index.html#autotoc_md18", null ],
-          [ "<tt>tuplelike<T></tt>", "index.html#autotoc_md19", null ],
-          [ "<tt>structured_bindable<T></tt>", "index.html#autotoc_md20", null ]
+      [ "Content", "index.html#autotoc_md14", [
+        [ "Aggregate-related concepts", "index.html#autotoc_md15", [
+          [ "<tt>unqualified_aggregate<T></tt>", "index.html#autotoc_md16", null ],
+          [ "<tt>aggregate<T></tt>", "index.html#autotoc_md17", null ],
+          [ "<tt>aggregate_constructible_from<T, args_ts...></tt>", "index.html#autotoc_md18", null ],
+          [ "<tt>aggregate_constructible_from_n_values<T, std::size_t N></tt>", "index.html#autotoc_md19", null ],
+          [ "<tt>tuplelike<T></tt>", "index.html#autotoc_md20", null ],
+          [ "<tt>structured_bindable<T></tt>", "index.html#autotoc_md21", null ]
         ] ],
-        [ "Aggregate-related type-traits", "index.html#autotoc_md21", [
-          [ "<tt>csl::ag::size<T></tt>", "index.html#autotoc_md22", null ],
-          [ "csl::ag::element<std::size_t, T>", "index.html#autotoc_md23", null ],
-          [ "csl::ag::view_element", "index.html#autotoc_md24", null ]
+        [ "Aggregate-related type-traits", "index.html#autotoc_md22", [
+          [ "<tt>csl::ag::size<T></tt>", "index.html#autotoc_md23", null ],
+          [ "csl::ag::element<std::size_t, T>", "index.html#autotoc_md24", null ],
+          [ "csl::ag::view_element", "index.html#autotoc_md25", null ]
         ] ],
-        [ "to-tuple conversion for aggregate types", "index.html#autotoc_md25", [
-          [ "Non-owning conversion (view)", "index.html#autotoc_md26", null ],
-          [ "Owning conversion", "index.html#autotoc_md27", null ]
+        [ "to-tuple conversion for aggregate types", "index.html#autotoc_md26", [
+          [ "Non-owning conversion (view)", "index.html#autotoc_md27", null ],
+          [ "Owning conversion", "index.html#autotoc_md28", null ]
         ] ],
-        [ "tuplelike interface for aggregates", "index.html#autotoc_md28", [
-          [ "std::tuple_element", "index.html#autotoc_md29", null ],
-          [ "std::get", "index.html#autotoc_md30", null ]
+        [ "tuplelike interface for aggregates", "index.html#autotoc_md29", [
+          [ "std::tuple_element", "index.html#autotoc_md30", null ],
+          [ "std::get", "index.html#autotoc_md31", null ]
         ] ],
-        [ "Pretty-printing", "index.html#autotoc_md31", [
-          [ "using std::ostream :", "index.html#autotoc_md32", null ]
+        [ "Pretty-printing", "index.html#autotoc_md32", [
+          [ "using std::ostream :", "index.html#autotoc_md33", null ]
         ] ]
       ] ],
-      [ "std::tuple and aggregate types homogeneity", "index.html#autotoc_md33", null ],
-      [ "Current limitations", "index.html#autotoc_md34", null ],
-      [ "(Internal details) Where's the magic ?", "index.html#autotoc_md35", null ]
+      [ "std::tuple and aggregate types homogeneity", "index.html#autotoc_md34", null ],
+      [ "Current limitations", "index.html#autotoc_md35", null ],
+      [ "(Internal details) Where's the magic ?", "index.html#autotoc_md36", null ]
     ] ],
     [ "Classes", "annotated.html", [
       [ "Class List", "annotated.html", "annotated_dup" ],
