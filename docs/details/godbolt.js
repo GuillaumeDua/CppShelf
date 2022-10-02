1ab9d707e5fad1e173e80eb0096528ed748ed442
GuillaumeDua <guillaume.dua@gmail.com>
2022-10-02 15:33:14 +0200

    [ag] docs : #75 : wip : theme selector

--- docs/details/godbolt.js
+++ docs/details/godbolt.js
@@ -1,3 +1,8 @@
+// code sections, code-color provided by highlightjs,
+// with send-to-godbolt and copy-to-clipboard buttons
+
+if (hljs === undefined)
+    console.error('godbolt.js depends on highlightjs, which is missing')
 
 class CopyToClipboardButton extends HTMLButtonElement {
 
@@ -41,6 +46,7 @@ customElements.define('copy-to-clipboard-button', CopyToClipboardButton, {extend
 
 function send_to_godbolt(element) {
     console.log('sending : ' + element)
+    // TODO: detect language
 }
 
 class SendToGodboltButton extends HTMLButtonElement {
@@ -51,11 +57,7 @@ class SendToGodboltButton extends HTMLButtonElement {
         <switch><g><path d="M58.6 46.5c-.3-.5-.3-1.2 0-1.7.3-.6.7-1.3 1-2 .2-.5-.1-1-.7-1h-5.8c-.6 0-1.2.3-1.4.8-.7 1.1-1.6 2.2-2.6 3.2-3.7 3.7-8.6 5.7-13.9 5.7-5.3 0-10.2-2-13.9-5.7-3.8-3.7-5.8-8.6-5.8-13.9s2-10.2 5.8-13.9c3.7-3.7 8.6-5.7 13.9-5.7 5.3 0 10.2 2 13.9 5.7 1 1 1.9 2.1 2.6 3.2.3.5.9.8 1.4.8h5.8c.5 0 .9-.5.7-1-.3-.7-.6-1.3-1-2-.3-.5-.3-1.2 0-1.7l1.9-3.5c.4-.7.3-1.5-.3-2.1l-4.9-4.9c-.6-.6-1.4-.7-2.1-.3l-3.6 2c-.5.3-1.2.3-1.7 0-1.7-.9-3.5-1.7-5.4-2.2-.6-.2-1-.6-1.2-1.2l-1.1-3.9C40.1.5 39.5 0 38.7 0h-6.9C31 0 30.2.5 30 1.3l-1.1 3.9c-.2.6-.6 1-1.2 1.2-1.9.6-3.6 1.3-5.3 2.2-.5.3-1.2.3-1.7 0l-3.6-2c-.7-.4-1.5-.3-2.1.3l-4.9 4.9c-.6.6-.7 1.4-.3 2.1l2 3.6c.3.5.3 1.2 0 1.7-.9 1.7-1.7 3.5-2.2 5.3-.2.6-.6 1-1.2 1.2l-3.9 1.1c-.7.2-1.3.9-1.3 1.7v6.9c0 .8.5 1.5 1.3 1.7l3.9 1.1c.6.2 1 .6 1.2 1.2.5 1.9 1.3 3.6 2.2 5.3.3.6.3 1.2 0 1.7l-2 3.6c-.4.7-.3 1.5.3 2.1L15 57c.6.6 1.4.7 2.1.3l3.6-2c.6-.3 1.2-.3 1.7 0 1.7.9 3.5 1.7 5.3 2.2.6.2 1 .6 1.2 1.2l1.1 3.9c.2.7.9 1.3 1.7 1.3h6.9c.8 0 1.5-.5 1.7-1.3l1.1-3.9c.2-.6.6-1 1.2-1.2 1.9-.6 3.6-1.3 5.4-2.2.5-.3 1.2-.3 1.7 0l3.6 2c.7.4 1.5.3 2.1-.3l4.9-4.9c.6-.6.7-1.4.3-2.1l-2-3.5z" fill="#67c52a"/><path d="M23.5 37.7v4.4h23.8v-4.4H23.5zm0-7.8v4.4h19.6v-4.4H23.5zm0-7.9v4.4h23.8V22H23.5z" fill="#3c3c3f"/>
         </g></switch></svg>`;
 
-    connectedCallback() {
-        console.log("SendToGodboltButton : connectedCallback")
-    }
     constructor() {
-        console.log("SendToGodboltButton : constructor")
 
         super();
         this.title = SendToGodboltButton.title;
@@ -103,7 +105,7 @@ class godbolt_snippet extends HTMLElement {
             console.log(`godbolt_snippet: Network Error`);
         };
         xhr.onload = function() {
-            console.log(`godbolt_snippet: Loaded with status: ${xhr.status}`);
+
             if (xhr.status != 200) {
                 return;
             }
@@ -115,6 +117,7 @@ class godbolt_snippet extends HTMLElement {
                 code_node.style.position = 'relative'
             let code = code_node.appendChild(document.createElement('code'));
                 code.textContent = _this.code
+                hljs.highlightElement(code)
 //                 code.textContent = `auto i = int{ 42 };
 // ++i; // comment
 // return generate_fun() + unicorns_everywhere<42>();`
@@ -128,17 +131,10 @@ class godbolt_snippet extends HTMLElement {
             code_node.appendChild(CE_button)
             _this.innerHTML = code_node.outerHTML;
 
-            hljs.highlightAll(); // TODO: only this one
+            // hljs.highlightAll(); // TODO: only this one
+            // hljs.highlightElement(code.innerHTML)
         };
         xhr.send();
-    }
-
-    static init() {
-        $(function() {
-            $(document).ready(function() {
-          
-            })
-        })
     }
 }
 window.customElements.define('godbolt-snippet', godbolt_snippet);
@@ -166,16 +162,94 @@ function inject_examples() {
     });
 }
 
-function godbolt_js_initialize() {
+class ThemeSelectorOptionElement extends HTMLOptionElement {
 
-    $(function() {
-        $(document).ready(function() {
-            console.log('initializing code sections ...')
-            inject_examples();
-        })
-    })
+    static url_base = 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.6.0/styles/'
+    static url_ext = '.min.css'
+    static dark_or_light_placeholder = '{dark_or_light}'
+
+    static BuildUrl(arg) {
+        if (typeof arg !== 'string' && ! arg instanceof String) {
+            console.error('ThemeSelectorOptionElement.BuildUrl: invalid argument')
+            return
+        }
+        return ThemeSelectorOptionElement.url_base
+            + arg + '-'
+            + ThemeSelectorOptionElement.dark_or_light_placeholder
+            + ThemeSelectorOptionElement.url_ext
+        ;
+    }
+
+    connectedCallback() {
+        console.log("ThemeSelectorOptionElement: connectedCallback")   
+    }
+
+    // todo: selected
+    constructor(arg) {
+        console.log("ThemeSelectorOptionElement: constructor: " + arg)
+
+        if (typeof arg === 'string' || arg instanceof String) {
+            this.text = arg
+            this.value = ThemeSelectorOptionElement.BuildUrl(arg)
+            return;
+        }
+        console.error('ThemeSelectorOptionElement: invalid argument')
+    }
 }
+customElements.define('theme-selector-option-element', ThemeSelectorOptionElement, {extends: 'option'});
+
+// Todo: href-integrity ?
+class ThemeSelector extends HTMLSelectElement { // extends HTMLSelectElement
+
+    connectedCallback() {
+
+        if (this.children.length == 0 || ! this.children.every(element => element instanceof ThemeSelectorOptionElement)) {
+            console.error('ThemeSelector: invalid argument (HTML)')
+            return
+        }
+        console.log('ThemeSelector: OK')
+    }
+
+    constructor(arg) {
+        super()
+
+        let theme_loader = document.createElement('link')
+            theme_loader.id = 'code_theme_stylesheet'
+            theme_loader.rel = 'stylesheet'
+            theme_loader.href = ""
+            theme_loader.integrity = ""
+            theme_loader.crossorigin = "anonymous"
+            theme_loader.referrerpolicy = "no-referrer"
+        this.appendChild(theme_loader)
+        
+        this.id = 'code_theme_selector'
+        this.onselectionchange = function() {
+            console.log('ThemeSelector: selection changed')
+        }
+
+        if (arg === undefined || ! Array.isArray(arg) || Array.length(arg) == 0) {
+            // console.error('ThemeSelector: constructor: invalid argument')
+        }
+        else {
+            arg.forEach(function(element) {
+                let option = new ThemeSelectorOptionElement(element)
+                this.appendChild(option)
+            });
+        }
+    }
+}
+customElements.define('theme-selector', ThemeSelector, {extends: 'select'});
 
 // TODO : highlightjs theme picker
 // - Tokyo Night Dark
 // - Base16/Google Dark
+
+class godbolt_js {
+
+    static initialize = function() {
+        $(document).ready(function() {
+            console.log('godbolt_js: initializing code sections ...')
+            inject_examples();
+        })
+    }
+}
