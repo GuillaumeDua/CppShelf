ddd21cfd34204015fdd19c714cb0a5eacaf177b2
GuillaumeDua <guillaume.dua@gmail.com>
2022-10-10 17:56:54 +0200

    [ag] docs : #75 : fix buttons, cleanup

--- docs/details/awesome-doc-code-sections.js
+++ docs/details/awesome-doc-code-sections.js
@@ -33,13 +33,17 @@
 //  - buttons :
 //      - send-to-godbolt
 //      - copy-to-clipboard
-//
-// TODO: as an external standalone project ? as a repo template ?
+
+// TODO: as an external standalone project ?
+// TODO: not mandatory dependency to doxygen
+// TODO: not mandatory dependency to doxygen-awesome-css
+// TODO: class that extends HTMLElement == div with specific class (as placeholders) + replace/init function (for Markdown for instance)
 
 if (hljs === undefined)
     console.error('awesome-doc-code-sections.js: depends on highlightjs, which is missing')
 
 class CopyToClipboardButton extends HTMLButtonElement {
+// Copy text context of this previousSibling HTMLelement
 
     static title        = "Copy to clipboard"
     static copyIcon     = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="32" height="32"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z"/></svg>`
@@ -60,6 +64,7 @@ class CopyToClipboardButton extends HTMLButtonElement {
         this.onclick = function(){ 
 
             this.innerHTML = CopyToClipboardButton.successIcon
+            this.style.fill = 'green'
 
             let text = this.previousSibling.textContent
             navigator.clipboard.writeText(text).then(
@@ -67,13 +72,13 @@ class CopyToClipboardButton extends HTMLButtonElement {
                     console.log('awesome-doc-code-sections.js:CopyToClipboardButton: success');
                 },
                 function(err) {
-                    console.error('CopyToClipboardButton: failed: ', err);
+                    console.error('awesome-doc-code-sections.js:CopyToClipboardButton: failed: ', err);
                 }
             );
             window.setTimeout(() => {
+                this.style.fill = 'black'
                 this.innerHTML = CopyToClipboardButton.copyIcon
-            }, CopyToClipboardButton.successDuration);    
-
+            }, CopyToClipboardButton.successDuration);
         }
     }
 }
@@ -85,12 +90,14 @@ function send_to_godbolt(element) {
 }
 
 class SendToGodboltButton extends HTMLButtonElement {
+// TODO: open a godbolt tab with code content, or replace code section by a godbolt iframe
 
     static title = 'Try this on godbolt.org (compiler-explorer)'
     static icon = 
         `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" width="32" height="32">
         <switch><g><path d="M58.6 46.5c-.3-.5-.3-1.2 0-1.7.3-.6.7-1.3 1-2 .2-.5-.1-1-.7-1h-5.8c-.6 0-1.2.3-1.4.8-.7 1.1-1.6 2.2-2.6 3.2-3.7 3.7-8.6 5.7-13.9 5.7-5.3 0-10.2-2-13.9-5.7-3.8-3.7-5.8-8.6-5.8-13.9s2-10.2 5.8-13.9c3.7-3.7 8.6-5.7 13.9-5.7 5.3 0 10.2 2 13.9 5.7 1 1 1.9 2.1 2.6 3.2.3.5.9.8 1.4.8h5.8c.5 0 .9-.5.7-1-.3-.7-.6-1.3-1-2-.3-.5-.3-1.2 0-1.7l1.9-3.5c.4-.7.3-1.5-.3-2.1l-4.9-4.9c-.6-.6-1.4-.7-2.1-.3l-3.6 2c-.5.3-1.2.3-1.7 0-1.7-.9-3.5-1.7-5.4-2.2-.6-.2-1-.6-1.2-1.2l-1.1-3.9C40.1.5 39.5 0 38.7 0h-6.9C31 0 30.2.5 30 1.3l-1.1 3.9c-.2.6-.6 1-1.2 1.2-1.9.6-3.6 1.3-5.3 2.2-.5.3-1.2.3-1.7 0l-3.6-2c-.7-.4-1.5-.3-2.1.3l-4.9 4.9c-.6.6-.7 1.4-.3 2.1l2 3.6c.3.5.3 1.2 0 1.7-.9 1.7-1.7 3.5-2.2 5.3-.2.6-.6 1-1.2 1.2l-3.9 1.1c-.7.2-1.3.9-1.3 1.7v6.9c0 .8.5 1.5 1.3 1.7l3.9 1.1c.6.2 1 .6 1.2 1.2.5 1.9 1.3 3.6 2.2 5.3.3.6.3 1.2 0 1.7l-2 3.6c-.4.7-.3 1.5.3 2.1L15 57c.6.6 1.4.7 2.1.3l3.6-2c.6-.3 1.2-.3 1.7 0 1.7.9 3.5 1.7 5.3 2.2.6.2 1 .6 1.2 1.2l1.1 3.9c.2.7.9 1.3 1.7 1.3h6.9c.8 0 1.5-.5 1.7-1.3l1.1-3.9c.2-.6.6-1 1.2-1.2 1.9-.6 3.6-1.3 5.4-2.2.5-.3 1.2-.3 1.7 0l3.6 2c.7.4 1.5.3 2.1-.3l4.9-4.9c.6-.6.7-1.4.3-2.1l-2-3.5z" fill="#67c52a"/><path d="M23.5 37.7v4.4h23.8v-4.4H23.5zm0-7.8v4.4h19.6v-4.4H23.5zm0-7.9v4.4h23.8V22H23.5z" fill="#3c3c3f"/>
         </g></switch></svg>`;
+    static successIcon  = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="32" height="32"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M9 16.17L4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41L9 16.17z"/></svg>`
 
     constructor() {
 
@@ -101,22 +108,35 @@ class SendToGodboltButton extends HTMLButtonElement {
         this.style.position = 'absolute';
         this.style.top = 10 + 'px';
         this.style.right = 10 + 'px';
+        this.style.borderRadius = 3
+
         this.addEventListener(
             'click',
-            () => console.log("awesome-doc-code-sections.js:SendToGodboltButton : clicked !")
+            () => {
+                console.log("awesome-doc-code-sections.js:SendToGodboltButton : clicked !")
+                this.innerHTML = SendToGodboltButton.successIcon
+                this.style.fill = 'green'
+
+                // TODO
+
+                window.setTimeout(() => {
+                    this.innerHTML = SendToGodboltButton.icon
+                    this.style.fill = 'black'
+                }, 1000);
+            }
         ); // TODO: godbolt CE API (and inject csl::ag header include<raw_github_path.hpp>)
     }
 }
 customElements.define('send-to-godbolt-button', SendToGodboltButton, {extends: 'button'});
 
-// Todo: Remove xhr (or make another component for remote ressources)
-//  local code had better to be injected
-//  - godbolt_snippet
-//  - godbolt_remote_snippet (or a factory for remote ressources that generates a godbolt_snippet)
-class godbolt_snippet extends HTMLElement {
+class RemoteCodeSection extends HTMLElement {
+// Fetch some code as texts based on the `code_url` parameter (or `url` attribute),
+// and creates a code-sections (pre/code) with synthax-color provided by hightlighthjs
+// Additionaly, the language code language can be forced (`code_language` parameter, or `language` attributes),
+// otherwise it is automatically detected based on fetched code content
 
     connectedCallback() {
-        // console.log('awesome-doc-code-sections.js:godbolt_snippet : connectedCallback with url attribute : ' + this.getAttribute('url'));
+        // console.log('awesome-doc-code-sections.js:RemoteCodeSection : connectedCallback with url attribute : ' + this.getAttribute('url'));
     }
 
     constructor(code_url, code_language) {
@@ -143,7 +163,7 @@ class godbolt_snippet extends HTMLElement {
         let xhr = new XMLHttpRequest();
         xhr.open('GET', this.code_url); // TODO: async
         xhr.onerror = function() {
-            console.error(`awesome-doc-code-sections.js:godbolt_snippet : Network Error`);
+            console.error(`awesome-doc-code-sections.js:RemoteCodeSection : Network Error`);
         };
         xhr.onload = function() {
 
@@ -180,9 +200,9 @@ class godbolt_snippet extends HTMLElement {
         xhr.send();
     }
 }
-window.customElements.define('godbolt-snippet', godbolt_snippet);
+window.customElements.define('remote-code-section', RemoteCodeSection);
 
-// TODO : same with old/previous table/tr/th/td ?
+// TODO : Optionaly wrap in table/tr/th/td ?
 
 class ThemeSelector {
 
@@ -249,10 +269,10 @@ class ThemeSelector {
 }
 
 var awesome_doc_code_sections = {}
-awesome_doc_code_sections.html_components = {}
-awesome_doc_code_sections.html_components.CopyToClipboardButton = CopyToClipboardButton
-awesome_doc_code_sections.html_components.SendToGodboltButton = SendToGodboltButton
-awesome_doc_code_sections.html_components.godbolt_snippet = godbolt_snippet
+awesome_doc_code_sections.HTML_elements = {}
+awesome_doc_code_sections.HTML_elements.CopyToClipboardButton = CopyToClipboardButton
+awesome_doc_code_sections.HTML_elements.SendToGodboltButton   = SendToGodboltButton
+awesome_doc_code_sections.HTML_elements.RemoteCodeSection     = RemoteCodeSection
 awesome_doc_code_sections.ThemeSelector = ThemeSelector // private?
 
 class ParsedCodeContent {
@@ -264,7 +284,7 @@ class ParsedCodeContent {
 }
 
 awesome_doc_code_sections.replace_doxygen_awesome_frament_wrapper = function() {
-// replace code-sections by doxygen-awesome-css
+// Replace code-sections generated by doxygen-awesome-css
 
     $(document).ready(function() {
         var place_holders = $('body').find('div[class=doxygen-awesome-fragment-wrapper]');
@@ -284,7 +304,7 @@ awesome_doc_code_sections.replace_doxygen_awesome_frament_wrapper = function() {
                 fragment.replaceWith(new_node)
             }
 
-            // TODO: remove doxygen-awesome-fragment-copy-button js file include
+            // TODO: remove doxygen-awesome-fragment-copy-button js file include ?
         });
     })
 }
@@ -303,7 +323,7 @@ awesome_doc_code_sections.inject_examples = () => { // private
 
         let language = (value.classList.length != 1 ? value.classList[1] : undefined);
 
-        let example_element = new godbolt_snippet(url, language);
+        let example_element = new RemoteCodeSection(url, language);
             example_element.setAttribute('url', url);
             example_element.setAttribute('language', language)
         value.appendChild(example_element);
