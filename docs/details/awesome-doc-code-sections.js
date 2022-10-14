b9c9d87e45334c3060dd71ce8759d965177bdad9
GuillaumeDua <guillaume.dua@gmail.com>
2022-10-14 18:23:45 +0200

    [ag] docs : #75 : ThemeSelector : minor fixes

--- docs/details/awesome-doc-code-sections.js
+++ docs/details/awesome-doc-code-sections.js
@@ -37,9 +37,11 @@
 //      - copy-to-clipboard
 
 // TODO: as an external standalone project ?
-// TODO: not mandatory dependency to doxygen
-// TODO: not mandatory dependency to doxygen-awesome-css
-// TODO: class that extends HTMLElement == div with specific class (as placeholders) + replace/init function (for Markdown for instance)
+// TODO: test behavior without theme selector
+// TODO: not mandatory dependency to doxygen             (WIP)
+// TODO: not mandatory dependency to doxygen-awesome-css (WIP)
+
+// TODO: fix custom HTMLElement constructor. For Buttons, test with <button is="typename" />
 
 if (hljs === undefined)
     console.error('awesome-doc-code-sections.js: depends on highlightjs, which is missing')
@@ -60,9 +62,6 @@ class ParsedCodeContent {
 
 // TODO: toggle dark/light mode if doxygen-awesome-css is not detected
 
-// TODO: Button: always visible vs. appear when mouseover the containing code section
-// TODO: Button: viewbox 0 0 24 24 width=24 height=24 ?
-
 class CopyToClipboardButton extends HTMLButtonElement {
 // Copy text context of this previousSibling HTMLelement
 
@@ -70,6 +69,7 @@ class CopyToClipboardButton extends HTMLButtonElement {
     static copyIcon     = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="32" height="32"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z"/></svg>`
     static successIcon  = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="32" height="32"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M9 16.17L4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41L9 16.17z"/></svg>`
     static successDuration = 980
+    static HTMLElement_name = 'awesome-doc-code-sections_el_copy-to-clipboard-button'
 
     constructor() {
         super();
@@ -103,7 +103,7 @@ class CopyToClipboardButton extends HTMLButtonElement {
         }
     }
 }
-customElements.define('awesome-doc-code-sections_copy-to-clipboard-button', CopyToClipboardButton, {extends: 'button'});
+customElements.define(CopyToClipboardButton.HTMLElement_name, CopyToClipboardButton, {extends: 'button'});
 
 function send_to_godbolt(element) {
     console.log('awesome-doc-code-sections.js:send_to_godbolt : ' + element)
@@ -120,10 +120,12 @@ class SendToGodboltButton extends HTMLButtonElement {
         <switch><g><path d="M58.6 46.5c-.3-.5-.3-1.2 0-1.7.3-.6.7-1.3 1-2 .2-.5-.1-1-.7-1h-5.8c-.6 0-1.2.3-1.4.8-.7 1.1-1.6 2.2-2.6 3.2-3.7 3.7-8.6 5.7-13.9 5.7-5.3 0-10.2-2-13.9-5.7-3.8-3.7-5.8-8.6-5.8-13.9s2-10.2 5.8-13.9c3.7-3.7 8.6-5.7 13.9-5.7 5.3 0 10.2 2 13.9 5.7 1 1 1.9 2.1 2.6 3.2.3.5.9.8 1.4.8h5.8c.5 0 .9-.5.7-1-.3-.7-.6-1.3-1-2-.3-.5-.3-1.2 0-1.7l1.9-3.5c.4-.7.3-1.5-.3-2.1l-4.9-4.9c-.6-.6-1.4-.7-2.1-.3l-3.6 2c-.5.3-1.2.3-1.7 0-1.7-.9-3.5-1.7-5.4-2.2-.6-.2-1-.6-1.2-1.2l-1.1-3.9C40.1.5 39.5 0 38.7 0h-6.9C31 0 30.2.5 30 1.3l-1.1 3.9c-.2.6-.6 1-1.2 1.2-1.9.6-3.6 1.3-5.3 2.2-.5.3-1.2.3-1.7 0l-3.6-2c-.7-.4-1.5-.3-2.1.3l-4.9 4.9c-.6.6-.7 1.4-.3 2.1l2 3.6c.3.5.3 1.2 0 1.7-.9 1.7-1.7 3.5-2.2 5.3-.2.6-.6 1-1.2 1.2l-3.9 1.1c-.7.2-1.3.9-1.3 1.7v6.9c0 .8.5 1.5 1.3 1.7l3.9 1.1c.6.2 1 .6 1.2 1.2.5 1.9 1.3 3.6 2.2 5.3.3.6.3 1.2 0 1.7l-2 3.6c-.4.7-.3 1.5.3 2.1L15 57c.6.6 1.4.7 2.1.3l3.6-2c.6-.3 1.2-.3 1.7 0 1.7.9 3.5 1.7 5.3 2.2.6.2 1 .6 1.2 1.2l1.1 3.9c.2.7.9 1.3 1.7 1.3h6.9c.8 0 1.5-.5 1.7-1.3l1.1-3.9c.2-.6.6-1 1.2-1.2 1.9-.6 3.6-1.3 5.4-2.2.5-.3 1.2-.3 1.7 0l3.6 2c.7.4 1.5.3 2.1-.3l4.9-4.9c.6-.6.7-1.4.3-2.1l-2-3.5z" fill="#67c52a"/><path d="M23.5 37.7v4.4h23.8v-4.4H23.5zm0-7.8v4.4h19.6v-4.4H23.5zm0-7.9v4.4h23.8V22H23.5z" fill="#3c3c3f"/>
         </g></switch></svg>`;
     static successIcon  = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="32" height="32"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M9 16.17L4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41L9 16.17z"/></svg>`
+    static HTMLElement_name = 'awesome-doc-code-sections_el_send-to-godbolt-button'
 
     constructor() {
 
         super();
+
         this.title = SendToGodboltButton.title;
         this.innerHTML = SendToGodboltButton.icon;
         this.style.zIndex = 2;
@@ -149,7 +151,7 @@ class SendToGodboltButton extends HTMLButtonElement {
         ); // TODO: godbolt CE API (and inject csl::ag header include<raw_github_path.hpp>)
     }
 }
-customElements.define('awesome-doc-code-sections_send-to-godbolt-button', SendToGodboltButton, {extends: 'button'});
+customElements.define(SendToGodboltButton.HTMLElement_name, SendToGodboltButton, {extends: 'button'});
 
 class CodeSection extends HTMLElement {
 // Code section, with synthax-coloration provided by highlightjs
@@ -232,10 +234,9 @@ class CodeSection extends HTMLElement {
         });
     }
 }
-window.customElements.define(CodeSection.HTMLElement_name, CodeSection);
+customElements.define(CodeSection.HTMLElement_name, CodeSection);
 
 // TODO : Optionaly wrap in table/tr/th/td ?
-// TODO: factory/init: replace all <div class='awesome-doc-code-sections_code-section' [language='cpp'] />
 
 class RemoteCodeSection extends CodeSection {
 // Fetch some code as texts based on the `code_url` parameter (or `url` attribute),
@@ -330,11 +331,7 @@ class RemoteCodeSection extends CodeSection {
         });
     }
 }
-window.customElements.define('awesome-doc-code-sections_remote-code-section', RemoteCodeSection);
-
-// TODO: factory/init: replace all <div class='awesome-doc-code-sections_remote-code-section' url='...' [language='cpp'] />
-
-// ============
+customElements.define(RemoteCodeSection.HTMLElement_name, RemoteCodeSection);
 
 class ThemeSelector {
 // For themes, see https://cdnjs.com/libraries/highlight.js
@@ -364,7 +361,7 @@ class ThemeSelector {
         ;
     }
 
-    static initialize = function() {
+    static Initialize_SelectHTMLElements = function() {
 
         let onOptionSelectedChange = function() {
             let selected_option = $(this).find('option:selected')
@@ -388,7 +385,7 @@ class ThemeSelector {
         $(document).ready(function() {
             console.log('awesome-doc-code-sections.js:ThemeSelector : initializing themes selector ...')
 
-            var options = $('body').find('option[class=code_theme_option]');
+            var options = $('body').find('select[class=code_theme_selector] option[class=code_theme_option]');
             options.each((index, element) => {
 
                 let value = element.getAttribute('value')
@@ -408,6 +405,8 @@ class ThemeSelector {
         })
     }
 }
+
+// ============
 
 var awesome_doc_code_sections = {}
     awesome_doc_code_sections.HTML_elements = {}
@@ -473,13 +472,15 @@ awesome_doc_code_sections.options = new class{
     doxygen_awesome_css_compatibility   = false
     pre_code_compatibility              = false
     auto_hide_buttons                   = false
+    toggle_dark_mode                    = (DoxygenAwesomeDarkModeToggle !== undefined)
 
     configure = function(obj) {
         if (obj === undefined || obj === null)
             return
-        this.doxygen_awesome_css_compatibility  = obj.doxygen_awesome_css_compatibility || false
-        this.pre_code_compatibility             = obj.pre_code_compatibility            || false
-        this.auto_hide_buttons                  = obj.auto_hide_buttons                 || false
+        this.doxygen_awesome_css_compatibility  = obj.doxygen_awesome_css_compatibility || this.doxygen_awesome_css_compatibility
+        this.pre_code_compatibility             = obj.pre_code_compatibility            || this.pre_code_compatibility
+        this.auto_hide_buttons                  = obj.auto_hide_buttons                 || this.auto_hide_buttons
+        this.toggle_dark_mode                   = obj.toggle_dark_mode                  || this.toggle_dark_mode
     }
 }()
 
@@ -500,7 +501,7 @@ awesome_doc_code_sections.initialize_ButtonsAutoHide = function() {
 
         let node_containing_button = $(value).parent().parent()
 
-        if (!node_containing_button.prop('nodeName').toLowerCase().startsWith("awesome-doc-code-sections_"))
+        if (!node_containing_button.prop('nodeName').toLowerCase().startsWith("awesome-doc-code-sections_cs_"))
             return // unlikely
 
         if (awesome_doc_code_sections.options.auto_hide_buttons
@@ -516,8 +517,9 @@ awesome_doc_code_sections.initialize = function() {
         $(document).ready(function() {
 
             console.log('awesome-doc-code-sections.js:initialize: initializing code sections ...')
-            awesome_doc_code_sections.ThemeSelector.initialize();
-            
+
+            // replace <select/> with proper HTML elements
+            awesome_doc_code_sections.ThemeSelector.Initialize_SelectHTMLElements();
             // replace <div/> with proper HTML elements
             awesome_doc_code_sections.HTML_elements.CodeSection.Initialize_DivHTMLElements();
             awesome_doc_code_sections.HTML_elements.RemoteCodeSection.Initialize_DivHTMLElements();
