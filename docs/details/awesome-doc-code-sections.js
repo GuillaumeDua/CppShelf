89ed2574e840314b858f68101e19f86ff9dec579
GuillaumeDua <guillaume.dua@gmail.com>
2022-10-24 19:14:14 +0200

    [ag] docs : #75 : well-partitioned toggle-dark-mode

--- docs/details/awesome-doc-code-sections.js
+++ docs/details/awesome-doc-code-sections.js
@@ -225,12 +225,12 @@ class ce_API {
         let promises_map = matches.map(async function(match) {
 
             let downloaded_file_content = await ce_API.#remote_files_cache.get(match[1])
-            console.log(`>>>>> fetched : [${downloaded_file_content}]`)
             code = code.replace(match[0], `// download[${match[0]}]::begin\n${downloaded_file_content}\n// download[${match[0]}]::end`)
         })
 
-        let fetch_result = () => {
-            // Build & send the request
+        // Build & send the request
+        let fetch_result = async () => {
+
             let body = {
                 "source": code,
                 "compiler": ce_options.compiler_id,
@@ -262,23 +262,18 @@ class ce_API {
 
             console.log(body)
 
-            fetch(`https://godbolt.org/api/compiler/${ce_options.compiler_id}/compile`, options)
+            return await fetch(`https://godbolt.org/api/compiler/${ce_options.compiler_id}/compile`, options)
                 .then(response => response.text())
-                .then(response => {
-                    console.log(response) // WIP
-                });
         }
 
         return await Promise.all(promises_map).then(() => {
-            fetch_result()
+            return fetch_result()
         })
     }
 }
 
 // ============
 // HTMLElements
-
-// TODO: toggle dark/light mode if doxygen-awesome-css is not detected
 
 class CopyToClipboardButton extends HTMLButtonElement {
 // Copy text context of this previousSibling HTMLelement
@@ -569,6 +564,9 @@ class CodeSection extends BasicCodeSection {
     #add_execution_panel() {
 
         ce_API.get_execution_result_for(this.ce_options, this.ce_code)
+            .then((result) => {
+                console.log('fetched: ' + result)
+            })
     }
 
     static Initialize_DivHTMLElements() {
@@ -702,7 +700,8 @@ class ThemeSelector {
 // The default one is the first one
 //
 // Use theme name, without light or dark specification
-// examples
+//
+// Example:
 //
 // <select class="code_theme_selector">
 //     <option class="code_theme_option" value="tokyo-night"></option>
@@ -749,11 +748,7 @@ class ThemeSelector {
 
             let html_node = document.getElementsByTagName('html')[0];
 
-            if (html_node.className !== "dark-mode" &&
-                html_node.className !== "light-mode")
-                return;
-
-            let theme_color = html_node.className.replace('-mode', '')
+            let theme_color = (html_node.classList.contains('dark-mode') ? 'dark' : 'light')
             let new_stylesheet_url = ThemeSelector.BuildUrl(selected_option.text())
                 .replace(ThemeSelector.dark_or_light_placeholder, theme_color)
             console.log('awesome-doc-code-sections.js:ThemeSelector : switching to stylesheet : ' + new_stylesheet_url)
@@ -798,7 +793,7 @@ const highlightjs_stylesheet_href_mutationObserver = new MutationObserver((mutat
         if (mutation.oldValue === code_stylesheet.href ||
             code_stylesheet.href === window.location.href)
             return
-        console.log('awesome-doc-code-sections_dark-mode.js:onHighlightjsHrefChange: Switching highlighths stylesheet \n from : ' + mutation.oldValue + '\n to   : ' + code_stylesheet.href)
+        console.log('awesome-doc-code-sections.js:onHighlightjsHrefChange: Switching highlighthjs stylesheet \n from : ' + mutation.oldValue + '\n to   : ' + code_stylesheet.href)
 
         hljs.highlightAll();
     })
@@ -812,145 +807,15 @@ highlightjs_stylesheet_href_mutationObserver.observe(
     }
 )
 
-// TODO: move to awesome-doc-code-sections_dark-mode.js
-class ToggleDarkMode /*StaticObserver*/ {
-// Handle dark/light mode info,
-// altering document class-list by adding `dark-mode` or `light-mode`
-//
-//  Note: DoxygenAwesomeDarkModeToggle is a great alternative to this,
-//        that you should use instead when using awesome-doc-code-sections in conjunction with DoxygenAwesomeCSS
-//        Both can be used at the same time though (compatiblity/inter-operability is a default feature)
-
-    static prefersLightModeInDarkModeKey    = "prefers-light-mode-in-dark-mode"
-    static prefersDarkModeInLightModeKey    = "prefers-dark-mode-in-light-mode"
-
-    static initialize = function() {
-
-        console.log(`awesome-doc-code-sections.js: ToggleDarkMode:initialize ...`)
-
-        // if (DoxygenAwesomeDarkModeToggle !== undefined) {
-        //     console.error('awesome-doc-code-sections.js:initialize: toggle light/dark mode : conflict with DoxygenAwesomeDarkModeToggle detected, aborting')
-        // }
-
-        ToggleDarkMode.enableDarkMode(ToggleDarkMode.userPreference)
-        window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
-            ToggleDarkMode.onSystemPreferenceChanged()
-        })
-        document.addEventListener("visibilitychange", visibilityState => {
-            if (document.visibilityState === 'visible') {
-                ToggleDarkMode.onSystemPreferenceChanged()
-            }
-        });
-    }
-
-    static enableDarkMode(enable) {
-        if (ToggleDarkMode.darkModeEnabled = enable) {
-            document.documentElement.classList.add("dark-mode")
-            document.documentElement.classList.remove("light-mode")
-        }
-        else {
-            document.documentElement.classList.remove("dark-mode")
-            document.documentElement.classList.add("light-mode")
-        }
-    }
-
-    static get systemPreference() {
-        return window.matchMedia('(prefers-color-scheme: dark)').matches
-    }
-
-    static get userPreference() {
-        return (
-            !ToggleDarkMode.systemPreference &&  localStorage.getItem(ToggleDarkMode.prefersDarkModeInLightModeKey)
-        ||   ToggleDarkMode.systemPreference && !localStorage.getItem(ToggleDarkMode.prefersLightModeInDarkModeKey)
-        );
-    }
-    static set userPreference(userPreference) {
-
-        ToggleDarkMode.darkModeEnabled = userPreference
-
-        if (!userPreference) {
-            if (ToggleDarkMode.systemPreference)
-                localStorage.setItem(ToggleDarkMode.prefersLightModeInDarkModeKey, true)
-            else
-                localStorage.removeItem(ToggleDarkMode.prefersDarkModeInLightModeKey)
-        }
-        else {
-            if (!ToggleDarkMode.systemPreference)
-                localStorage.setItem(ToggleDarkMode.prefersDarkModeInLightModeKey, true)
-            else
-                localStorage.removeItem(ToggleDarkMode.prefersLightModeInDarkModeKey)
-        }
-        ToggleDarkMode.onUserPreferenceChanged()
-    }
-
-    static onSystemPreferenceChanged() {
-        ToggleDarkMode.darkModeEnabled = ToggleDarkMode.userPreference
-        ToggleDarkMode.enableDarkMode(ToggleDarkMode.darkModeEnabled)
-    }
-    static onUserPreferenceChanged() {
-        ToggleDarkMode.enableDarkMode(ToggleDarkMode.darkModeEnabled)
-    }
-}
-class ToggleDarkModeButton extends HTMLButtonElement {
-
-    static HTMLElement_name                 = "awesome-doc-code-sections_toggle-dark-mode-button"
-
-    static lightModeIcon                    = `<svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px" fill="#FCBF00"><rect fill="none" height="24" width="24"/><circle cx="12" cy="12" opacity=".3" r="3"/><path d="M12,9c1.65,0,3,1.35,3,3s-1.35,3-3,3s-3-1.35-3-3S10.35,9,12,9 M12,7c-2.76,0-5,2.24-5,5s2.24,5,5,5s5-2.24,5-5 S14.76,7,12,7L12,7z M2,13l2,0c0.55,0,1-0.45,1-1s-0.45-1-1-1l-2,0c-0.55,0-1,0.45-1,1S1.45,13,2,13z M20,13l2,0c0.55,0,1-0.45,1-1 s-0.45-1-1-1l-2,0c-0.55,0-1,0.45-1,1S19.45,13,20,13z M11,2v2c0,0.55,0.45,1,1,1s1-0.45,1-1V2c0-0.55-0.45-1-1-1S11,1.45,11,2z M11,20v2c0,0.55,0.45,1,1,1s1-0.45,1-1v-2c0-0.55-0.45-1-1-1C11.45,19,11,19.45,11,20z M5.99,4.58c-0.39-0.39-1.03-0.39-1.41,0 c-0.39,0.39-0.39,1.03,0,1.41l1.06,1.06c0.39,0.39,1.03,0.39,1.41,0s0.39-1.03,0-1.41L5.99,4.58z M18.36,16.95 c-0.39-0.39-1.03-0.39-1.41,0c-0.39,0.39-0.39,1.03,0,1.41l1.06,1.06c0.39,0.39,1.03,0.39,1.41,0c0.39-0.39,0.39-1.03,0-1.41 L18.36,16.95z M19.42,5.99c0.39-0.39,0.39-1.03,0-1.41c-0.39-0.39-1.03-0.39-1.41,0l-1.06,1.06c-0.39,0.39-0.39,1.03,0,1.41 s1.03,0.39,1.41,0L19.42,5.99z M7.05,18.36c0.39-0.39,0.39-1.03,0-1.41c-0.39-0.39-1.03-0.39-1.41,0l-1.06,1.06 c-0.39,0.39-0.39,1.03,0,1.41s1.03,0.39,1.41,0L7.05,18.36z"/></svg>`
-    static darkModeIcon                     = `<svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px" fill="#FE9700"><rect fill="none" height="24" width="24"/><path d="M9.37,5.51C9.19,6.15,9.1,6.82,9.1,7.5c0,4.08,3.32,7.4,7.4,7.4c0.68,0,1.35-0.09,1.99-0.27 C17.45,17.19,14.93,19,12,19c-3.86,0-7-3.14-7-7C5,9.07,6.81,6.55,9.37,5.51z" opacity=".3"/><path d="M9.37,5.51C9.19,6.15,9.1,6.82,9.1,7.5c0,4.08,3.32,7.4,7.4,7.4c0.68,0,1.35-0.09,1.99-0.27C17.45,17.19,14.93,19,12,19 c-3.86,0-7-3.14-7-7C5,9.07,6.81,6.55,9.37,5.51z M12,3c-4.97,0-9,4.03-9,9s4.03,9,9,9s9-4.03,9-9c0-0.46-0.04-0.92-0.1-1.36 c-0.98,1.37-2.58,2.26-4.4,2.26c-2.98,0-5.4-2.42-5.4-5.4c0-1.81,0.89-3.42,2.26-4.4C12.92,3.04,12.46,3,12,3L12,3z"/></svg>`
-    static title                            = "Toggle Light/Dark Mode"
-
-    toggleMode() {
-        ToggleDarkMode.userPreference = !ToggleDarkMode.userPreference
-        this.updateIcon()
-    }
-
-    constructor() {
-        super()
-
-        let _this = this
-        $(function() {
-            $(document).ready(function() {
-                
-                _this.title = ToggleDarkModeButton.title
-                _this.addEventListener('click', _this.toggleMode);
-                _this.updateIcon()
-
-                window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
-                    _this.updateIcon()
-                })
-                window.matchMedia('(prefers-color-scheme: light)').addEventListener('change', event => {
-                    _this.updateIcon()
-                })
-                document.addEventListener("visibilitychange", visibilityState => {
-                    if (document.visibilityState === 'visible') {
-                        _this.updateIcon()
-                    }
-                });
-            })
-        })
-    }
-
-    updateIcon() {
-
-        this.innerHTML = ToggleDarkMode.darkModeEnabled
-            ? ToggleDarkModeButton.darkModeIcon
-            : ToggleDarkModeButton.lightModeIcon
-        ;
-    }
-}
-customElements.define(ToggleDarkModeButton.HTMLElement_name, ToggleDarkModeButton, {extends: 'button'});
-
 // ============
 
 awesome_doc_code_sections.HTML_elements = {}
 awesome_doc_code_sections.HTML_elements.CopyToClipboardButton = CopyToClipboardButton
 awesome_doc_code_sections.HTML_elements.SendToGodboltButton   = SendToGodboltButton
-awesome_doc_code_sections.HTML_elements.ToggleDarkModeButton  = ToggleDarkModeButton
 awesome_doc_code_sections.HTML_elements.BasicCodeSection      = BasicCodeSection
 awesome_doc_code_sections.HTML_elements.CodeSection           = CodeSection
 awesome_doc_code_sections.HTML_elements.RemoteCodeSection     = RemoteCodeSection
 awesome_doc_code_sections.ThemeSelector = ThemeSelector
-awesome_doc_code_sections.ToggleDarkMode = ToggleDarkMode
 
 // TODO: make sure that doxygen elements are also still clickable with pure doxygen (not doxygen-awesome-css)
 awesome_doc_code_sections.initialize_doxygenCodeSections = function() {
@@ -1087,7 +952,15 @@ awesome_doc_code_sections.initialize = function() {
 
             console.log('awesome-doc-code-sections.js:initialize: initializing code sections ...')
 
-            awesome_doc_code_sections.ToggleDarkMode.initialize()
+            if (awesome_doc_code_sections.options.toggle_dark_mode) {
+                if (undefined === awesome_doc_code_sections.ToggleDarkMode)
+                    console.error(
+                        'awesome-doc-code-sections.js:initialize: options toggle_dark_mode set to true, but awesome_doc_code_sections.ToggleDarkMode is undefined\n' +
+                        'Did you forget to include awesome-doc-code-sections_dark-mode.js ?'
+                    )
+                else
+                    awesome_doc_code_sections.ToggleDarkMode.initialize()
+            }
 
             // replace <select/> with proper HTML elements
             awesome_doc_code_sections.ThemeSelector.Initialize_SelectHTMLElements();
--- docs/details/awesome-doc-code-sections_dark-mode.js
+++ docs/details/awesome-doc-code-sections_dark-mode.js
@@ -32,8 +32,136 @@ if (typeof awesome_doc_code_sections === 'undefined')
 
 // ============
 
+class ToggleDarkMode /*StaticObserver*/ {
+// Handle dark/light mode info,
+// altering document class-list by adding `dark-mode` or `light-mode`
+//
+//  Note: DoxygenAwesomeDarkModeToggle is a great alternative to this,
+//        that you should use instead when using awesome-doc-code-sections in conjunction with DoxygenAwesomeCSS
+//        Both can be used at the same time though (compatiblity/inter-operability is a default feature)
+
+    static prefersLightModeInDarkModeKey    = "prefers-light-mode-in-dark-mode"
+    static prefersDarkModeInLightModeKey    = "prefers-dark-mode-in-light-mode"
+
+    static initialize = function() {
+
+        console.log(`awesome-doc-code-sections.js: ToggleDarkMode:initialize ...`)
+
+        // if (DoxygenAwesomeDarkModeToggle !== undefined) {
+        //     console.error('awesome-doc-code-sections.js:initialize: toggle light/dark mode : conflict with DoxygenAwesomeDarkModeToggle detected, aborting')
+        // }
+
+        ToggleDarkMode.enableDarkMode(ToggleDarkMode.userPreference)
+        window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
+            ToggleDarkMode.onSystemPreferenceChanged()
+        })
+        document.addEventListener("visibilitychange", visibilityState => {
+            if (document.visibilityState === 'visible') {
+                ToggleDarkMode.onSystemPreferenceChanged()
+            }
+        });
+    }
+
+    static enableDarkMode(enable) {
+        if (ToggleDarkMode.darkModeEnabled = enable) {
+            document.documentElement.classList.add("dark-mode")
+            document.documentElement.classList.remove("light-mode")
+        }
+        else {
+            document.documentElement.classList.remove("dark-mode")
+            document.documentElement.classList.add("light-mode")
+        }
+    }
+
+    static get systemPreference() {
+        return window.matchMedia('(prefers-color-scheme: dark)').matches
+    }
+
+    static get userPreference() {
+        return (
+            !ToggleDarkMode.systemPreference &&  localStorage.getItem(ToggleDarkMode.prefersDarkModeInLightModeKey)
+        ||   ToggleDarkMode.systemPreference && !localStorage.getItem(ToggleDarkMode.prefersLightModeInDarkModeKey)
+        );
+    }
+    static set userPreference(userPreference) {
+
+        ToggleDarkMode.darkModeEnabled = userPreference
+
+        if (!userPreference) {
+            if (ToggleDarkMode.systemPreference)
+                localStorage.setItem(ToggleDarkMode.prefersLightModeInDarkModeKey, true)
+            else
+                localStorage.removeItem(ToggleDarkMode.prefersDarkModeInLightModeKey)
+        }
+        else {
+            if (!ToggleDarkMode.systemPreference)
+                localStorage.setItem(ToggleDarkMode.prefersDarkModeInLightModeKey, true)
+            else
+                localStorage.removeItem(ToggleDarkMode.prefersLightModeInDarkModeKey)
+        }
+        ToggleDarkMode.onUserPreferenceChanged()
+    }
+
+    static onSystemPreferenceChanged() {
+        ToggleDarkMode.darkModeEnabled = ToggleDarkMode.userPreference
+        ToggleDarkMode.enableDarkMode(ToggleDarkMode.darkModeEnabled)
+    }
+    static onUserPreferenceChanged() {
+        ToggleDarkMode.enableDarkMode(ToggleDarkMode.darkModeEnabled)
+    }
+}
+awesome_doc_code_sections.ToggleDarkMode = ToggleDarkMode
+class ToggleDarkModeButton extends HTMLButtonElement {
+
+    static HTMLElement_name                 = "awesome-doc-code-sections_toggle-dark-mode-button"
+
+    static lightModeIcon                    = `<svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px" fill="#FCBF00"><rect fill="none" height="24" width="24"/><circle cx="12" cy="12" opacity=".3" r="3"/><path d="M12,9c1.65,0,3,1.35,3,3s-1.35,3-3,3s-3-1.35-3-3S10.35,9,12,9 M12,7c-2.76,0-5,2.24-5,5s2.24,5,5,5s5-2.24,5-5 S14.76,7,12,7L12,7z M2,13l2,0c0.55,0,1-0.45,1-1s-0.45-1-1-1l-2,0c-0.55,0-1,0.45-1,1S1.45,13,2,13z M20,13l2,0c0.55,0,1-0.45,1-1 s-0.45-1-1-1l-2,0c-0.55,0-1,0.45-1,1S19.45,13,20,13z M11,2v2c0,0.55,0.45,1,1,1s1-0.45,1-1V2c0-0.55-0.45-1-1-1S11,1.45,11,2z M11,20v2c0,0.55,0.45,1,1,1s1-0.45,1-1v-2c0-0.55-0.45-1-1-1C11.45,19,11,19.45,11,20z M5.99,4.58c-0.39-0.39-1.03-0.39-1.41,0 c-0.39,0.39-0.39,1.03,0,1.41l1.06,1.06c0.39,0.39,1.03,0.39,1.41,0s0.39-1.03,0-1.41L5.99,4.58z M18.36,16.95 c-0.39-0.39-1.03-0.39-1.41,0c-0.39,0.39-0.39,1.03,0,1.41l1.06,1.06c0.39,0.39,1.03,0.39,1.41,0c0.39-0.39,0.39-1.03,0-1.41 L18.36,16.95z M19.42,5.99c0.39-0.39,0.39-1.03,0-1.41c-0.39-0.39-1.03-0.39-1.41,0l-1.06,1.06c-0.39,0.39-0.39,1.03,0,1.41 s1.03,0.39,1.41,0L19.42,5.99z M7.05,18.36c0.39-0.39,0.39-1.03,0-1.41c-0.39-0.39-1.03-0.39-1.41,0l-1.06,1.06 c-0.39,0.39-0.39,1.03,0,1.41s1.03,0.39,1.41,0L7.05,18.36z"/></svg>`
+    static darkModeIcon                     = `<svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px" fill="#FE9700"><rect fill="none" height="24" width="24"/><path d="M9.37,5.51C9.19,6.15,9.1,6.82,9.1,7.5c0,4.08,3.32,7.4,7.4,7.4c0.68,0,1.35-0.09,1.99-0.27 C17.45,17.19,14.93,19,12,19c-3.86,0-7-3.14-7-7C5,9.07,6.81,6.55,9.37,5.51z" opacity=".3"/><path d="M9.37,5.51C9.19,6.15,9.1,6.82,9.1,7.5c0,4.08,3.32,7.4,7.4,7.4c0.68,0,1.35-0.09,1.99-0.27C17.45,17.19,14.93,19,12,19 c-3.86,0-7-3.14-7-7C5,9.07,6.81,6.55,9.37,5.51z M12,3c-4.97,0-9,4.03-9,9s4.03,9,9,9s9-4.03,9-9c0-0.46-0.04-0.92-0.1-1.36 c-0.98,1.37-2.58,2.26-4.4,2.26c-2.98,0-5.4-2.42-5.4-5.4c0-1.81,0.89-3.42,2.26-4.4C12.92,3.04,12.46,3,12,3L12,3z"/></svg>`
+    static title                            = "Toggle Light/Dark Mode"
+
+    toggleMode() {
+        ToggleDarkMode.userPreference = !ToggleDarkMode.userPreference
+        this.updateIcon()
+    }
+
+    constructor() {
+        super()
+
+        let _this = this
+        $(function() {
+            $(document).ready(function() {
+                
+                _this.title = ToggleDarkModeButton.title
+                _this.addEventListener('click', _this.toggleMode);
+                _this.updateIcon()
+
+                window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
+                    _this.updateIcon()
+                })
+                window.matchMedia('(prefers-color-scheme: light)').addEventListener('change', event => {
+                    _this.updateIcon()
+                })
+                document.addEventListener("visibilitychange", visibilityState => {
+                    if (document.visibilityState === 'visible') {
+                        _this.updateIcon()
+                    }
+                });
+            })
+        })
+    }
+
+    updateIcon() {
+
+        this.innerHTML = ToggleDarkMode.darkModeEnabled
+            ? ToggleDarkModeButton.darkModeIcon
+            : ToggleDarkModeButton.lightModeIcon
+        ;
+    }
+}
+customElements.define(ToggleDarkModeButton.HTMLElement_name, ToggleDarkModeButton, {extends: 'button'});
+awesome_doc_code_sections.HTML_elements.ToggleDarkModeButton  = ToggleDarkModeButton
+    
 // light/dark theme switch
-awesome_doc_code_sections.ThemeSelector.can_handle_darkMode = true
 awesome_doc_code_sections.ThemeSelector.toggleDarkLightMode_onTheme = function(isDarkModeEnabled) {
     
     let code_stylesheet = document.getElementById(awesome_doc_code_sections.ThemeSelector.stylesheet_HTML_placeholder_id);
@@ -93,6 +221,3 @@ html_class_mutationObserver.observe(
         attributeOldValue: true
     }
 );
-
-
-// TODO: move other related stuffs here ...
