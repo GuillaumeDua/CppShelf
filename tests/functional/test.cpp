d5777ceb0c195fd69aaf8c118a2f3087892954b0
GuillaumeDua <guillaume.dua@gmail.com>
2023-09-28 23:33:21 +0200

    [functional] #127 - csl::functional::function_trait: C++17 retro-compatibility - better testing

--- tests/functional/test.cpp
+++ tests/functional/test.cpp
@@ -1,28 +1,69 @@
 #include <csl/functional.hpp>
 
+namespace test::sample {
+
+    [[maybe_unused]] static const auto lambda_const = [](int) -> char{ return {}; };
+    using lambda_const_t = decltype(lambda_const);
+    [[maybe_unused]] static const auto lambda_mutable = [](int) -> char{ return {}; };
+    using lambda_mutable_t = decltype(lambda_mutable);
+    [[maybe_unused]] static const auto lambda_const_noexcept = [](int) -> char{ return {}; };
+    using lambda_const_noexcept_t = decltype(lambda_const_noexcept);
+
+    [[maybe_unused]] auto func(int) -> char { return {}; }
+    [[maybe_unused]] auto func_noexcept(int) noexcept -> char { return {}; }
+
+    struct user_defined_type {
+        // NOLINTBEGIN
+        [[maybe_unused]] auto mem_func(int) -> char { return {}; }
+        [[maybe_unused]] auto mem_func_noexcept(int) noexcept -> char { return {}; }
+        [[maybe_unused]] auto mem_func_const(int) const -> char { return {}; }
+        [[maybe_unused]] auto mem_func_const_noexcept(int) const noexcept -> char { return {}; }
+        // NOLINTEND
+        [[maybe_unused]] static auto static_mem_func(int) -> char { return {}; }
+        [[maybe_unused]] static auto static_mem_func_noexcept(int) noexcept -> char { return {}; }
+    };
+}
+
 namespace test::function {
 
-    auto func(int){ return 'a'; }
-    using trait = csl::functional::function_trait<decltype(func)>;
+    using trait = csl::functional::function_trait<decltype(sample::func)>;
     static_assert(std::is_same_v<trait::arguments_type, csl::functional::arguments<int>>);
     static_assert(std::is_same_v<trait::result_type, char>);
 }
 namespace test::member_function {
-    struct toto {
-        // TODO: ref-qualified mem-func
-        auto func(int){ return 'a'; } // NOLINT(readability-convert-member-functions-to-static)
-    };
-    using trait = csl::functional::function_trait<decltype(&toto::func)>;
-    static_assert(std::is_same_v<trait::arguments_type, csl::functional::arguments<toto, int>>);
+    using trait = csl::functional::function_trait<decltype(&sample::user_defined_type::mem_func)>;
+    static_assert(std::is_same_v<trait::arguments_type, csl::functional::arguments<sample::user_defined_type, int>>);
     static_assert(std::is_same_v<trait::result_type, char>);
 }
 namespace test::lambda {
 
-    [[maybe_unused]] const auto func = [](int){ return 'a'; };
-
-    using trait = csl::functional::function_trait<decltype(func)>;
+    using trait = csl::functional::function_trait<sample::lambda_const_t>;
     static_assert(std::is_same_v<trait::arguments_type, csl::functional::arguments<int>>);
     static_assert(std::is_same_v<trait::result_type, char>);
 }
+namespace test::concepts {
+
+    namespace ns = csl::functional::concepts;
+
+    // simple_callable
+    static_assert(ns::simple_callable<decltype(sample::lambda_const)>);
+    static_assert(ns::simple_callable<decltype(sample::lambda_mutable)>);
+    static_assert(ns::simple_callable<decltype(sample::lambda_const_noexcept)>);
+    static_assert(ns::simple_callable<decltype(sample::func)>);
+    static_assert(ns::simple_callable<decltype(sample::func_noexcept)>);
+    static_assert(ns::simple_callable<decltype(&sample::user_defined_type::mem_func)>);
+    static_assert(ns::simple_callable<decltype(&sample::user_defined_type::mem_func_noexcept)>);
+    static_assert(ns::simple_callable<decltype(&sample::user_defined_type::mem_func_const)>);
+    static_assert(ns::simple_callable<decltype(&sample::user_defined_type::mem_func_const_noexcept)>);
+    static_assert(ns::simple_callable<decltype(&sample::user_defined_type::static_mem_func)>);
+    static_assert(ns::simple_callable<decltype(&sample::user_defined_type::static_mem_func_noexcept)>);
+
+    // invocable_with
+
+    // nothrow_invocable_with
+}
+
+// TODO(Guss): overload
 
 auto main() -> int {}
+
