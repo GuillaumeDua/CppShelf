fc009996ae54fbd6a104e9fe3ad9b5221f5623a3
GuillaumeDua <guillaume.dua@gmail.com>
2023-10-16 23:56:46 +0200

    [ag] #131 - ensure bitfield support ON+OFF tests

--- tests/ag/test.cpp
+++ tests/ag/test.cpp
@@ -1,4 +1,4 @@
-#include <type_traits>
+// Force CSL_AG_ENABLE_BITFIELDS_SUPPORT=OFF
 #if defined(CSL_AG_ENABLE_BITFIELDS_SUPPORT)
 # undef CSL_AG_ENABLE_BITFIELDS_SUPPORT
 #endif
@@ -8,46 +8,9 @@
 #endif
 
 #include "includes/tests/ag/details/mp.hpp"
-#include "includes/tests/ag/size.hpp"
 #include "includes/tests/ag/conversion/to_tuple.hpp"
 #include "includes/tests/ag/conversion/to_tuple_view.hpp"
-
-#include <iostream>
-#include <iomanip>
-#include <cassert>
-
-
-void print(csl::ag::concepts::aggregate auto && value){
-
-    using type = std::remove_cvref_t<decltype(value)>;
-
-    std::cout
-        << "--------------------------------------------\n"
-        << "| " << gcl::cx::type_name_v<decltype(value)> << '\n'
-        << "--------------------------------------------\n"
-    ;
-
-    [&]<
-        typename ... Ts,
-        std::size_t ... indexes
-    >(
-        std::type_identity<std::tuple<Ts...>>,
-        std::index_sequence<indexes...>
-    ){
-        ((
-            std::cout
-                << std::left
-                << std::setw(12) << gcl::cx::type_name_v<csl::ag::element_t<indexes, type>>
-                << " -> "
-                << std::setw(12) << gcl::cx::type_name_v<decltype(csl::ag::get<indexes>(csl_fwd(value)))>
-                << " -> "
-                << std::setw(12) << gcl::cx::type_name_v<decltype(csl::ag::get<Ts>(csl_fwd(value)))>
-                << "\n"
-        ), ...);
-    }(
-        std::type_identity<csl::ag::to_tuple_t<type>>{},
-        std::make_index_sequence<csl::ag::size_v<type>>{}
-    );
-}
+#include "includes/tests/ag/size.hpp"
+#include "includes/tests/ag/get.hpp"
 
 auto main() -> int {}
--- tests/ag/test_bitfield_support.cpp
+++ tests/ag/test_bitfield_support.cpp
@@ -1,7 +1,12 @@
-#define CSL_AG_ENABLE_BITFIELDS_SUPPORT true // force bitfield support. Prefer using CMake cache instead CSL_AG_ENABLE_BITFIELDS_SUPPORT=ON
+// Force CSL_AG_ENABLE_BITFIELDS_SUPPORT=ON
+#if not defined(CSL_AG_ENABLE_BITFIELDS_SUPPORT)
+# define CSL_AG_ENABLE_BITFIELDS_SUPPORT true // force bitfield support. Prefer using CMake cache instead CSL_AG_ENABLE_BITFIELDS_SUPPORT=ON
+#endif
 #include <csl/ag.hpp>
+#if not defined(CSL_AG_ENABLE_BITFIELDS_SUPPORT)
+# error "[Test] csl::ag : expect CSL_AG_ENABLE_BITFIELDS_SUPPORT=OFF"
+#endif
 
 #include "includes/tests/ag/size.hpp"
 
-auto main() -> int {
-}
+auto main() -> int {}
