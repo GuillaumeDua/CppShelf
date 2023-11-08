30197a4b03492cf396eea9b8db98ab6fa62fcb89
GuillaumeDua <guillaume.dua@gmail.com>
2023-11-08 23:15:16 +0100

    [ag] remove deprecated test (tuple_interface)

--- tests/ag/test.cpp
+++ tests/ag/test.cpp
@@ -7,13 +7,12 @@
 # error "[Test] csl::ag : expect CSL_AG_ENABLE_BITFIELDS_SUPPORT=OFF"
 #endif
 
-#include "includes/tests/ag/details/mp.hpp"
-#include "includes/tests/ag/conversion/to_tuple.hpp"
-#include "includes/tests/ag/conversion/to_tuple_view.hpp"
-#include "includes/tests/ag/conversion/make.hpp"
-#include "includes/tests/ag/size.hpp"
-#include "includes/tests/ag/element.hpp"
-#include "includes/tests/ag/get.hpp"
-// TODO: tuple_interface
+#include "tests/ag/details/mp.hpp"
+#include "tests/ag/conversion/to_tuple.hpp"
+#include "tests/ag/conversion/to_tuple_view.hpp"
+#include "tests/ag/conversion/make.hpp"
+#include "tests/ag/size.hpp"
+#include "tests/ag/element.hpp"
+#include "tests/ag/get.hpp"
 
 auto main() -> int {}
