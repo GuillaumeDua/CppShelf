93e35f9d6d425489139bae76cb91dde58deca095
GuillaumeDua <pro.guillaume.dua@gmail.com>
2024-07-06 10:10:05 +0200

    [fix/CI/llvm_broken_pipe] #221 - fixing attempt

--- .devcontainer/scripts/llvm.sh
+++ .devcontainer/scripts/llvm.sh
@@ -247,10 +247,8 @@ codename=$(lsb_release -cs)
 mapfile -t llvm_versions_to_install < <(echo -n "$llvm_versions")
 for version in "${llvm_versions_to_install[@]}"; do
 
-    trap '' PIPE
-    yes '' | ./${internal_script_path} ${version} all -n ${codename} > /dev/null 2>&1 \
+    (yes '' ||: ) | ./${internal_script_path} ${version} all -n ${codename} > /dev/null 2>&1 \
     || error "running [${external_script_url} ${version} all] failed"
-    trap - PIPE
 
     # Warning: only one installation of `lldb` is allowed by `apt` at a time. Cannot use `--no-remove` here
     # apt install -qq -y --no-install-recommends \
