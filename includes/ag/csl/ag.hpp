a7f0e9197c83fec5e29450b1418e9e5fd8305217
GuillaumeDua <pro.guillaume.dua@gmail.com>
2024-09-17 22:01:42 +0200

    [ag] #242 - compile-time constant indentation (WIP)

--- includes/ag/csl/ag.hpp
+++ includes/ag/csl/ag.hpp
@@ -1135,7 +1135,7 @@ namespace csl::ag::io::presentation {
         // Poor's man alternative: https://godbolt.org/z/aGq6qGaK9
         // or https://godbolt.org/z/hqY7MMEK8
         // WIP: requires type erasure, user-defined string-likes ... https://godbolt.org/z/vK9KaYYvG
-        //  WTF simple solution: https://godbolt.org/z/oWYs64jMY ???
+        //  WTF simple solution: https://godbolt.org/z/8W6Ybn6h4 ???
 
         explicit constexpr indented(std::size_t depth)
         : indentation{make_filled_basic_string_view<Char{' '}>((depth + 1) * 3)}
