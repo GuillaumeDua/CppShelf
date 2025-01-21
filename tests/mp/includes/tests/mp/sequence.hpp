60a84ab65a31c2c74d7ebb52c9e0189b83711e83
GuillaumeDua <pro.guillaume.dua@gmail.com>
2025-01-21 19:13:55 +0100

    [mp] #96 tuple storage: fixing seq API

--- tests/mp/includes/tests/mp/sequence.hpp
+++ tests/mp/includes/tests/mp/sequence.hpp
@@ -2,22 +2,39 @@
 #include <csl/mp.hpp>
 
 // sequences
-namespace test::reverse_integer_sequence {
+namespace test::reverse_::ordered_ {
     
     using namespace csl::mp::seq;
 
-    using seq_type = std::make_index_sequence<5>;                           // NOLINT(cppcoreguidelines-avoid-magic-numbers)
-    using reversed_seq_type = csl::mp::seq::make_reverse_index_sequence<5>; // NOLINT(cppcoreguidelines-avoid-magic-numbers)
+    using type = std::make_index_sequence<5>;                           // NOLINT(cppcoreguidelines-avoid-magic-numbers)
+    using reversed_type = csl::mp::seq::make_reverse_index_sequence<5>; // NOLINT(cppcoreguidelines-avoid-magic-numbers)
     static_assert(std::same_as<
-        reversed_seq_type,
+        reversed_type,
         std::index_sequence<4,3,2,1,0>
     >);
     static_assert(std::same_as<
-        reverse_sequence<reversed_seq_type>,
+        reverse<reversed_type>,
         std::index_sequence<0,1,2,3,4>
     >);
     static_assert(std::same_as<
-        reversed_seq_type,
-        reverse_sequence<std::index_sequence<0,1,2,3,4>>
+        reversed_type,
+        reverse<std::index_sequence<0,1,2,3,4>>
     >);
+}
+namespace test::reverse_::unordered_ {
+    using namespace csl::mp::seq;
+
+    using type = std::index_sequence<4,1,0,3,2>;                           // NOLINT(cppcoreguidelines-avoid-magic-numbers)
+    static_assert(std::same_as<
+        reverse<type>,
+        std::index_sequence<2,3,0,1,4>
+    >);
+}
+
+
+namespace test::value_at_ {
+
+}
+namespace test::get_ {
+
 }
