ef9577aff66fbc6e05797e335b5fa5a1d7dacf3a
GuillaumeDua <pro.guillaume.dua@gmail.com>
2025-06-24 22:09:28 +0200

    [testing] mini-framework

---
+++ tests/common/includes/common/test.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#if defined(__cpp_lib_source_location) and __cpp_lib_source_location <=	201907L
+# include <source_location>
+using source_location_t = std::source_location;
+#elif __has_include(<experimental/source_location>)
+#include <experimental/source_location>
+using source_location_t = std::experimental::source_location;
+#else
+# error "missing <source_location>"
+#endif
+
+#include <stdexcept>
+
+namespace test {
+    struct failure : std::runtime_error {
+        explicit failure(source_location_t location = source_location_t::current())
+        : std::runtime_error(location.function_name())
+        {}
+        using std::runtime_error::what;
+    };
+}
