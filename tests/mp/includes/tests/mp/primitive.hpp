cc9e2a2025ee734aba16b39c734d33c4a9f5304c
GuillaumeDua <pro.guillaume.dua@gmail.com>
2025-09-08 21:40:26 +0200

    [mp] test - fwd_ref

--- tests/mp/includes/tests/mp/primitive.hpp
+++ tests/mp/includes/tests/mp/primitive.hpp
@@ -8,7 +8,10 @@ namespace test::primitives::type_identity {
     static_assert(std::same_as<int, csl::mp::type_identity_t<int>>);
 }
 namespace test::primitives::value_type {
-
+    static_assert(csl::mp::concepts::fwd_ref<int&, int>);
+    static_assert(csl::mp::concepts::fwd_ref<int&&, int>);
+    static_assert(csl::mp::concepts::fwd_ref<const int&, int>);
+    static_assert(csl::mp::concepts::fwd_ref<const int&&, int>);
 }
 namespace test::primitives::bind_front {
 
