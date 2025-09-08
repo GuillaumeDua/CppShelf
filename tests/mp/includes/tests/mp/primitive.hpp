28be15d857c40a5640ab9f3b3b0b1a45dd920f2d
GuillaumeDua <pro.guillaume.dua@gmail.com>
2025-09-08 21:45:04 +0200

    [mp] test - unwrap_ref

--- tests/mp/includes/tests/mp/primitive.hpp
+++ tests/mp/includes/tests/mp/primitive.hpp
@@ -7,14 +7,28 @@
 namespace test::primitives::type_identity {
     static_assert(std::same_as<int, csl::mp::type_identity_t<int>>);
 }
-namespace test::primitives::value_type {
+namespace test::concepts::fwd_ref {
     static_assert(csl::mp::concepts::fwd_ref<int&, int>);
     static_assert(csl::mp::concepts::fwd_ref<int&&, int>);
     static_assert(csl::mp::concepts::fwd_ref<const int&, int>);
     static_assert(csl::mp::concepts::fwd_ref<const int&&, int>);
+
+    static_assert(not csl::mp::concepts::fwd_ref<int&, int&>);
+}
+namespace test::concepts::unwrap_reference {
+    static_assert(std::same_as<int, csl::mp::unwrap_reference_t<int>>);
+    static_assert(std::same_as<int&, csl::mp::unwrap_reference_t<std::reference_wrapper<int>>>);
+    static_assert(not std::same_as<int, csl::mp::unwrap_reference_t<int&>>);
+
+    static_assert(std::same_as<int, csl::mp::unwrap_ref_decay_t<int>>);
+    static_assert(std::same_as<int&, csl::mp::unwrap_ref_decay_t<std::reference_wrapper<int>>>);
+    static_assert(std::same_as<int, csl::mp::unwrap_ref_decay_t<int&>>);
+}
+
+namespace test::primitives::value_type {
 }
 namespace test::primitives::bind_front {
-
+    
 }
 namespace test::primitives::bind_back {
 
