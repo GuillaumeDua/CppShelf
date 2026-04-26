43c74a10cb6cce2a9bf8e925bcad45d732d1df3a
GuillaumeDua <pro.guillaume.dua@gmail.com>
2026-04-26 12:54:50 +0200

    [csl::test] cleanup, namespace

--- tests/common/includes/common/test.hpp
+++ tests/common/includes/common/test.hpp
@@ -1,32 +1,40 @@
 #pragma once
 
-// mini C++17 testing framework: https://godbolt.org/z/sTb6aza87,
-// which may support <source_location> if available.
-// For instance, not when using C++17 with libc++
+// Minimalistic C++17 testing framework: https://godbolt.org/z/sTb6aza87,
+//  supports <source_location> if available.
+//  For instance, not when using C++17 with libc++
+//
+// Prefer Catch2 or GoogleTest when available.
+
+#if __cplusplus < 201703L
+ #error "csl/mp.hpp requires C++17 or greater"
+#endif
 
 #if defined(__cpp_lib_source_location) and __cpp_lib_source_location <=	201907L
 # include <source_location>
-namespace test::details {
+namespace csl::test::details {
 using source_location = std::source_location;
 }
 #elif __has_include(<experimental/source_location>)
+#pragma message("csl::test: using <experimental/source_location>. Prefer <location> if available.")
 #include <experimental/source_location>
-namespace test::details {
+namespace csl::test::details {
 using source_location = std::experimental::source_location;
 }
 #else
-namespace test::details {
+namespace csl::test::details {
     using source_location = void;
 }
 #endif
 
 #include <stdexcept>
 
-namespace test {
+namespace csl::test {
     struct failure : std::runtime_error {
 
+        // NOLINTBEGIN(modernize-use-constraints)
         template <
-            typename source_location = test::details::source_location,
+            typename source_location = csl::test::details::source_location,
             std::enable_if_t<not std::is_void_v<source_location>, int> = 0
         >
         explicit failure(source_location location = source_location::current())
@@ -34,12 +42,13 @@ namespace test {
         {}
 
         template <
-            typename source_location = test::details::source_location,
+            typename source_location = csl::test::details::source_location,
             std::enable_if_t<std::is_void_v<source_location>, int> = 0
         >
         explicit failure()
-        : std::runtime_error("location not supported")
+        : std::runtime_error("<source_location> not supported")
         {}
+        // NOLINTEND(modernize-use-constraints)
 
         using std::runtime_error::runtime_error;
         using std::runtime_error::what;
