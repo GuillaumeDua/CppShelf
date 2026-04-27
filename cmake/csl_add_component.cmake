09f69414ce1570e5d78536dff9f17ec32e4e4469
GuillaumeDua <pro.guillaume.dua@gmail.com>
2026-04-27 12:24:22 +0200

    [#306] link warnings as a dedicated target

--- cmake/csl_add_component.cmake
+++ cmake/csl_add_component.cmake
@@ -1,5 +1,3 @@
-option(CSL_STRICT_WARNINGS "[${PROJECT_NAME}] strict warnings" OFF)
-
 include(CMakeParseArguments)
 include(${CMAKE_CURRENT_SOURCE_DIR}/cmake/print_aligned.cmake)
 
@@ -7,7 +5,7 @@ include(${CMAKE_CURRENT_SOURCE_DIR}/cmake/print_aligned.cmake)
 function(csl_add_component)
 
     set(options)
-    set(oneValueArgs COMPONENT)
+    set(oneValueArgs NAME)
     set(multiValueArgs)
     cmake_parse_arguments(csl_add_component
         "${options}"
@@ -23,47 +21,31 @@ function(csl_add_component)
         message(FATAL_ERROR "[csl_add_component] error: KEYWORDS_MISSING_VALUES = [${csl_add_component_KEYWORDS_MISSING_VALUES}]")
     endif()
 
-    message(STATUS "[${PROJECT_NAME}::${csl_add_component_COMPONENT}]")
+    set(csl_add_component_NAME ${csl_add_component_NAME} PARENT_SCOPE)
 
-    add_library(${PROJECT_NAME}_${csl_add_component_COMPONENT} INTERFACE)
-    add_library(${PROJECT_NAME}::${csl_add_component_COMPONENT} ALIAS ${PROJECT_NAME}_${csl_add_component_COMPONENT})
+    message(STATUS "[${PROJECT_NAME}::${csl_add_component_NAME}]")
 
-    target_compile_options(csl_${csl_add_component_COMPONENT} INTERFACE
+    add_library(${PROJECT_NAME}_${csl_add_component_NAME} INTERFACE)
+    add_library(${PROJECT_NAME}::${csl_add_component_NAME} ALIAS ${PROJECT_NAME}_${csl_add_component_NAME})
 
-        $<$<BOOL:${CSL_STRICT_WARNINGS}>:-Werror >
-        # -Wdouble-promotion
-        # -Wpedantic -pedantic-errors
-
-        -Wall -Wextra
-        -Wshadow
-    #   -Wno-pointer-sign
-        -Wunused-variable
-        -Woverloaded-virtual
-        -Wno-unknown-pragmas
-        -Wno-unused-parameter
-        -Wnull-dereference
-        -Wconversion
+    target_include_directories(csl_${csl_add_component_NAME} INTERFACE
+        ${PROJECT_SOURCE_DIR}/libs/${csl_add_component_NAME}/includes/${csl_add_component_NAME}
     )
 
-    target_include_directories(csl_${csl_add_component_COMPONENT} INTERFACE
-        ${PROJECT_SOURCE_DIR}/libs/${csl_add_component_COMPONENT}/includes/${csl_add_component_COMPONENT}
-    )
-
-    set(component_name ${csl_add_component_COMPONENT})
-    set(_csl_options_cmake "${PROJECT_SOURCE_DIR}/libs/${csl_add_component_COMPONENT}/cmake/options.cmake")
+    set(_csl_options_cmake "${PROJECT_SOURCE_DIR}/libs/${csl_add_component_NAME}/cmake/options.cmake")
     if (EXISTS "${_csl_options_cmake}")
-        message(VERBOSE "[${PROJECT_NAME}] csl::${csl_add_component_COMPONENT} : loading options from ${_csl_options_cmake}")
+        message(VERBOSE "[${PROJECT_NAME}] csl::${csl_add_component_NAME} : loading options from ${_csl_options_cmake}")
         list(APPEND CMAKE_MESSAGE_INDENT "  ")
         include("${_csl_options_cmake}")
         list(POP_BACK CMAKE_MESSAGE_INDENT)
     endif()
 
     # test
-    if (CSL_TEST_${csl_add_component_COMPONENT})
-        set(_csl_tests_dir "${PROJECT_SOURCE_DIR}/libs/${csl_add_component_COMPONENT}/tests")
+    if (CSL_TEST_${csl_add_component_NAME})
+        set(_csl_tests_dir "${PROJECT_SOURCE_DIR}/libs/${csl_add_component_NAME}/tests")
         if (EXISTS "${_csl_tests_dir}/CMakeLists.txt")
             enable_testing()
-            message(VERBOSE "[${PROJECT_NAME}] csl::${csl_add_component_COMPONENT} (tests)")
+            message(VERBOSE "[${PROJECT_NAME}] csl::${csl_add_component_NAME} (tests)")
             list(APPEND CMAKE_MESSAGE_INDENT "   ")
             add_subdirectory(${_csl_tests_dir})
             list(POP_BACK CMAKE_MESSAGE_INDENT)
@@ -71,10 +53,10 @@ function(csl_add_component)
     endif()
 
     # example
-    if (CSL_EXAMPLE_${csl_add_component_COMPONENT})
-        set(_csl_examples_dir "${PROJECT_SOURCE_DIR}/libs/${csl_add_component_COMPONENT}/examples")
+    if (CSL_EXAMPLE_${csl_add_component_NAME})
+        set(_csl_examples_dir "${PROJECT_SOURCE_DIR}/libs/${csl_add_component_NAME}/examples")
         if (EXISTS "${_csl_examples_dir}/CMakeLists.txt")
-            message(VERBOSE "[${PROJECT_NAME}] csl::${csl_add_component_COMPONENT} (example)")
+            message(VERBOSE "[${PROJECT_NAME}] csl::${csl_add_component_NAME} (example)")
             list(APPEND CMAKE_MESSAGE_INDENT "   ")
             add_subdirectory(${_csl_examples_dir})
             list(POP_BACK CMAKE_MESSAGE_INDENT)
