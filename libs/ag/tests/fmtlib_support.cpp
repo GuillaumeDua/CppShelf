015c3b3ad43ab4b9775c6f8e595f3be5fe2b66f4
GuillaumeDua <pro.guillaume.dua@gmail.com>
2026-05-04 21:30:44 +0200

    [#310] migrating GoogleTest to Catch2, using CPM everywhere (WIP)

--- libs/ag/tests/fmtlib_support.cpp
+++ libs/ag/tests/fmtlib_support.cpp
@@ -53,53 +53,47 @@ namespace tests::concepts::fmt_formattable {
     static_assert(concepts::fmt_formattable<std::array<int, 3>, char>);
 }
 
-#include <gtest/gtest.h>
+#include <catch2/catch_template_test_macros.hpp>
+#include <catch2/catch_test_macros.hpp>
 
-using test_models = ::testing::Types<
-    types::field_1,
-    types::field_2,
-    types::field_3_nested,
-    types::field_3_nested_tuplelike,
-    types::field_4_nested_range,
-    types::field_everything
->;
-TYPED_TEST_SUITE(csl_test_ag_FmtFormatAggregate, test_models);
+namespace {
+    template <typename T>
+    struct fixture;
 
-template <typename T>
-struct csl_test_ag_FmtFormatAggregate : public testing::Test {};
-#pragma region csl_test_ag_FmtFormatAggregate specializations
-template <>
-struct csl_test_ag_FmtFormatAggregate<types::field_1> : public testing::Test {
-    constexpr static types::field_1 value{ .i = 42 };
-    constexpr static std::string_view default_formatter_expected_result = "{42}";
-    constexpr static std::string_view default_formatter_n_expected_result = "42";
-    constexpr static std::string_view indented_formatter_expected_result =
+#pragma region fixture specializations
+    template <>
+    struct fixture<types::field_1> {
+        constexpr static types::field_1 value{ .i = 42 };
+        constexpr static std::string_view default_formatter_expected_result = "{42}";
+        constexpr static std::string_view default_formatter_n_expected_result = "42";
+        constexpr static std::string_view indented_formatter_expected_result =
 R"({
     42
 })";
-};
+    };
 
-template <>
-struct csl_test_ag_FmtFormatAggregate<types::field_2> : public testing::Test {
-    constexpr static types::field_2 value{ .i = 123, .c = 'A' };
-    constexpr static std::string_view default_formatter_expected_result = "{123, 'A'}";
-    constexpr static std::string_view default_formatter_n_expected_result = "123'A'";
-    constexpr static std::string_view indented_formatter_expected_result =
+    template <>
+    struct fixture<types::field_2> {
+        constexpr static types::field_2 value{ .i = 123, .c = 'A' };
+        constexpr static std::string_view default_formatter_expected_result = "{123, 'A'}";
+        constexpr static std::string_view default_formatter_n_expected_result = "123'A'";
+        constexpr static std::string_view indented_formatter_expected_result =
 R"({
     123,
     'A'
 })";
-};
-template <>
-struct csl_test_ag_FmtFormatAggregate<types::field_3_nested> : public testing::Test {
-    constexpr static types::field_3_nested value{
-        .i = 1,
-        .f1 = csl_test_ag_FmtFormatAggregate<types::field_1>::value,
-        .f2 = csl_test_ag_FmtFormatAggregate<types::field_2>::value
     };
-    constexpr static std::string_view default_formatter_expected_result = "{1, {42}, {123, 'A'}}";
-    constexpr static std::string_view default_formatter_n_expected_result = "1{42}{123, 'A'}";
-    constexpr static std::string_view indented_formatter_expected_result =
+
+    template <>
+    struct fixture<types::field_3_nested> {
+        constexpr static types::field_3_nested value{
+            .i = 1,
+            .f1 = fixture<types::field_1>::value,
+            .f2 = fixture<types::field_2>::value
+        };
+        constexpr static std::string_view default_formatter_expected_result = "{1, {42}, {123, 'A'}}";
+        constexpr static std::string_view default_formatter_n_expected_result = "1{42}{123, 'A'}";
+        constexpr static std::string_view indented_formatter_expected_result =
 R"({
     1,
     {
@@ -110,17 +104,18 @@ R"({
         'A'
     }
 })";
-};
-template <>
-struct csl_test_ag_FmtFormatAggregate<types::field_3_nested_tuplelike> : public testing::Test {
-    constexpr static types::field_3_nested_tuplelike value{
-        .tu = { 2, 'b', "str"},
-        .a = {'a', 'b', 'c'},
-        .p = { 42, 43 }, // NOLINT
     };
-    constexpr static std::string_view default_formatter_expected_result = R"({(2, 'b', "str"), ['a', 'b', 'c'], (42, 43)})";
-    constexpr static std::string_view default_formatter_n_expected_result = R"((2, 'b', "str")['a', 'b', 'c'](42, 43))";
-    constexpr static std::string_view indented_formatter_expected_result =
+
+    template <>
+    struct fixture<types::field_3_nested_tuplelike> {
+        constexpr static types::field_3_nested_tuplelike value{
+            .tu = { 2, 'b', "str"},
+            .a = {'a', 'b', 'c'},
+            .p = { 42, 43 }, // NOLINT
+        };
+        constexpr static std::string_view default_formatter_expected_result = R"({(2, 'b', "str"), ['a', 'b', 'c'], (42, 43)})";
+        constexpr static std::string_view default_formatter_n_expected_result = R"((2, 'b', "str")['a', 'b', 'c'](42, 43))";
+        constexpr static std::string_view indented_formatter_expected_result =
 R"({
     (
         2,
@@ -137,18 +132,19 @@ R"({
         43
     )
 })";
-};
-template <>
-struct csl_test_ag_FmtFormatAggregate<types::field_4_nested_range> : public testing::Test {
-    constexpr static types::field_4_nested_range value{
-        .sv = "hello",
-        .a_c = { 'a', 'b', 'c' },
-        .a_i = { 42, 43, 44 },
-        .a_sv = { "a", "b", "c" },
     };
-    constexpr static std::string_view default_formatter_expected_result = R"({"hello", ['a', 'b', 'c'], [42, 43, 44], ["a", "b", "c"]})";
-    constexpr static std::string_view default_formatter_n_expected_result = R"("hello"['a', 'b', 'c'][42, 43, 44]["a", "b", "c"])";
-    constexpr static std::string_view indented_formatter_expected_result =
+
+    template <>
+    struct fixture<types::field_4_nested_range> {
+        constexpr static types::field_4_nested_range value{
+            .sv = "hello",
+            .a_c = { 'a', 'b', 'c' },
+            .a_i = { 42, 43, 44 },
+            .a_sv = { "a", "b", "c" },
+        };
+        constexpr static std::string_view default_formatter_expected_result = R"({"hello", ['a', 'b', 'c'], [42, 43, 44], ["a", "b", "c"]})";
+        constexpr static std::string_view default_formatter_n_expected_result = R"("hello"['a', 'b', 'c'][42, 43, 44]["a", "b", "c"])";
+        constexpr static std::string_view indented_formatter_expected_result =
 R"({
     "hello",
     [
@@ -167,19 +163,19 @@ R"({
         "c"
     ]
 })";
-};
-template <>
-struct csl_test_ag_FmtFormatAggregate<types::field_everything> : public testing::Test {
-
-    constexpr static types::field_everything value{
-        .b = true,
-        .f1 = csl_test_ag_FmtFormatAggregate<types::field_3_nested>::value,
-        .f2 = csl_test_ag_FmtFormatAggregate<types::field_3_nested_tuplelike>::value,
-        .f3 = csl_test_ag_FmtFormatAggregate<types::field_4_nested_range>::value,
     };
-    constexpr static std::string_view default_formatter_expected_result = R"({true, {1, {42}, {123, 'A'}}, {(2, 'b', "str"), ['a', 'b', 'c'], (42, 43)}, {"hello", ['a', 'b', 'c'], [42, 43, 44], ["a", "b", "c"]}})";
-    constexpr static std::string_view default_formatter_n_expected_result = R"(true{1, {42}, {123, 'A'}}{(2, 'b', "str"), ['a', 'b', 'c'], (42, 43)}{"hello", ['a', 'b', 'c'], [42, 43, 44], ["a", "b", "c"]})";
-    constexpr static std::string_view indented_formatter_expected_result =
+
+    template <>
+    struct fixture<types::field_everything> {
+        constexpr static types::field_everything value{
+            .b = true,
+            .f1 = fixture<types::field_3_nested>::value,
+            .f2 = fixture<types::field_3_nested_tuplelike>::value,
+            .f3 = fixture<types::field_4_nested_range>::value,
+        };
+        constexpr static std::string_view default_formatter_expected_result = R"({true, {1, {42}, {123, 'A'}}, {(2, 'b', "str"), ['a', 'b', 'c'], (42, 43)}, {"hello", ['a', 'b', 'c'], [42, 43, 44], ["a", "b", "c"]}})";
+        constexpr static std::string_view default_formatter_n_expected_result = R"(true{1, {42}, {123, 'A'}}{(2, 'b', "str"), ['a', 'b', 'c'], (42, 43)}{"hello", ['a', 'b', 'c'], [42, 43, 44], ["a", "b", "c"]})";
+        constexpr static std::string_view indented_formatter_expected_result =
 R"({
     true,
     {
@@ -227,56 +223,46 @@ R"({
         ]
     }
 })";
-};
-
+    };
 #pragma endregion
+} // namespace
 
-TYPED_TEST(csl_test_ag_FmtFormatAggregate, DefaultFormatter) {
-
-    EXPECT_EQ(
-        fmt::format("{}", TestFixture::value),
-        TestFixture::default_formatter_expected_result
-    );
-}
-TYPED_TEST(csl_test_ag_FmtFormatAggregate, DefaultFormatterParseN) {
-
-    EXPECT_EQ(
-        fmt::format("{:n}", TestFixture::value),
-        TestFixture::default_formatter_n_expected_result
-    );
-}
-TYPED_TEST(csl_test_ag_FmtFormatAggregate, IndentedFormatter) {
-
-    EXPECT_EQ(
-        fmt::format("{}", TestFixture::value | csl::ag::io::indented),
-        TestFixture::indented_formatter_expected_result
-    );
-    EXPECT_EQ(
-        fmt::format("{}", csl::ag::io::details::decorators::depthen_view_t{ TestFixture::value }),
-        TestFixture::indented_formatter_expected_result
-    );
+TEMPLATE_TEST_CASE("csl::ag::fmt default formatter", "[ag][fmt]",
+    types::field_1,
+    types::field_2,
+    types::field_3_nested,
+    types::field_3_nested_tuplelike,
+    types::field_4_nested_range,
+    types::field_everything
+) {
+    using f = fixture<TestType>;
+    CHECK(fmt::format("{}", f::value) == f::default_formatter_expected_result);
 }
 
+TEMPLATE_TEST_CASE("csl::ag::fmt default formatter :n", "[ag][fmt]",
+    types::field_1,
+    types::field_2,
+    types::field_3_nested,
+    types::field_3_nested_tuplelike,
+    types::field_4_nested_range,
+    types::field_everything
+) {
+    using f = fixture<TestType>;
+    CHECK(fmt::format("{:n}", f::value) == f::default_formatter_n_expected_result);
+}
 
-auto main(int argc, char *argv[]) -> int {
-
-    // WIP: https://godbolt.org/z/ovv8eoqq8
-    // const auto value = types::field_3_nested{
-    //     .i = 1,
-    //     .f1 = {
-    //         11,
-    //     },
-    //     .f2 = {
-    //         .i = 12,
-    //         .c = 'c'
-    //     }
-    // };
-    // fmt::println("compact    : [{:c}]", value);
-    // fmt::println("pretty     : [\n{:i}\n]", value);
-    // fmt::println("pretty(I)  : [\n{:i,I}\n]", value);
-    // fmt::println("pretty(T)  : [\n{:i,T}\n]", value);
-    // fmt::println("pretty(IT) : [\n{:i,IT}\n]", value);
-
-    testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+TEMPLATE_TEST_CASE("csl::ag::fmt indented formatter", "[ag][fmt]",
+    types::field_1,
+    types::field_2,
+    types::field_3_nested,
+    types::field_3_nested_tuplelike,
+    types::field_4_nested_range,
+    types::field_everything
+) {
+    using f = fixture<TestType>;
+    CHECK(fmt::format("{}", f::value | csl::ag::io::indented) == f::indented_formatter_expected_result);
+    CHECK(
+        fmt::format("{}", csl::ag::io::details::decorators::depthen_view_t{ f::value })
+        == f::indented_formatter_expected_result
+    );
 }
