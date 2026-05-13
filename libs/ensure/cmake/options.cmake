aac2e212301cb20c426944c6ccbd6f938f9e74d3
GuillaumeDua <pro.guillaume.dua@gmail.com>
2026-05-13 10:06:24 +0200

    [#310] csl::ensure: fetch fmt using CPM instead of fetchContent

--- libs/ensure/cmake/options.cmake
+++ libs/ensure/cmake/options.cmake
@@ -10,20 +10,13 @@ option(CSL_ENSURE__ENABLE_FMT_SUPPORT "[${CMAKE_PROJECT_NAME}::${csl_add_compone
 csl_print_aligned(STATUS CSL_ENSURE__ENABLE_FMT_SUPPORT)
 if (${CSL_ENSURE__ENABLE_FMT_SUPPORT})
 
-    # REFACTO: use CPM
-    # 3rd party: fmt
-    set(FETCHCONTENT_QUIET ON)
-    message(STATUS "[${CMAKE_PROJECT_NAME}::${csl_add_component_NAME}] fetching [fmt] library ...")
-   
-    if (NOT TARGET fmt::fmt-header-only)
-        list(APPEND CMAKE_MESSAGE_INDENT "  ")
-        include(FetchContent)
-        FetchContent_Declare(fmt
-            GIT_REPOSITORY  https://github.com/fmtlib/fmt.git
-            GIT_TAG         main
+    if (NOT TARGET fmt::fmt-header-only AND NOT TARGET fmt::fmt)
+        include(csl/get_cpm)
+        CPMAddPackage(
+            NAME              fmt
+            GITHUB_REPOSITORY fmtlib/fmt
+            GIT_TAG           12.1.0
         )
-        FetchContent_MakeAvailable(fmt)
-        list(POP_BACK CMAKE_MESSAGE_INDENT)
     endif()
 
     target_compile_definitions(csl_${csl_add_component_NAME} INTERFACE CSL_ENSURE__ENABLE_FMT_SUPPORT)
