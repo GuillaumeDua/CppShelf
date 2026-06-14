e194c922f035b4fa10af4299e760b206bfe4f1d3
GuillaumeDua <pro.guillaume.dua@gmail.com>
2026-06-14 21:23:55 +0200

    [#343][ag] Formatting support - fmtlib, std::format examples

---
+++ libs/ag/examples/23_formatting_std_format_simple.cpp
@@ -0,0 +1,21 @@
+#define CSL_AG__ENABLE_STD_FORMAT_SUPPORT 1
+
+#include <csl/ag.hpp>
+#include <format>
+#include <iostream>
+
+auto main() -> int {
+    using namespace csl::ag::io;
+
+    struct A { int i; char c; };
+    const A value{ .i = 42, .c = 'x' };
+
+    // NOTE: prefer std::print, if available
+    std::cout
+        << std::format("{}\n", value)
+        << std::format("{}\n", value | indented)
+        << std::format("{}\n", value | indexed)
+        << std::format("{}\n", value | typenamed)
+        << std::format("{}\n", value | indented | indexed | typenamed)
+    ;
+}
