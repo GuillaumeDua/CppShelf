4bbecfc11b7e150407efa45e11c4ad35a92876d5
GuillaumeDua <pro.guillaume.dua@gmail.com>
2026-06-15 11:11:24 +0200

    [#144][typeinfo] cleaner CPO design, cleaner API

--- libs/typeinfo/includes/typeinfo/csl/typeinfo.hpp
+++ libs/typeinfo/includes/typeinfo/csl/typeinfo.hpp
@@ -38,34 +38,9 @@ namespace csl::typeinfo::details
 #endif
         return value;
     }
-}
-
-namespace csl::typeinfo
-{   // constexpr typeinfo that does not relies on __cpp_rtti
-    // [Warning]: Produced outputs ARE NOT portable: inconsistencies exist across compilers (GCC, Clang, msvc-cl)
-    //
-    // Known limitations :
-    //
-    //  [type_name]
-    //     decl namespace (demo: https://godbolt.org/z/vhr4h4j9Y)
-    //            GCC:    namespace is part of the type (ex: A::B::C::type_as_ns<>::func<>(int)::my_type)
-    //            clang:  missing (ex: my_type)
-    //     type aliases (demo: https://godbolt.org/z/9PEq9zWTn)
-    //        ex : std::string
-    //            std::basic_string<char>
-    //            std::__cxx11::basic_string<char>
-    //            std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char>>
-    //     MSVC: "class "/"struct " prefixes stripped at the top level only
-    //
-    //  [value_name]
-    //     values representation (demo: https://godbolt.org/z/bn9ofo3Pz)
-    //      ex : int{42}
-    //          GCC, Clang: "42"sv
-    //          MSVC:       "0x2a"sv
-    //      use <charconv> std::to_chars into std::string_view for reliable basic numerical values
 
     template <typename T>
-    [[nodiscard]] constexpr /*consteval*/ static auto type_name(/*no parameters allowed*/) -> std::string_view
+    [[nodiscard]] constexpr /*consteval*/ static auto make_type_name(/*no parameters allowed*/) -> std::string_view
     {
 #if defined(__GNUC__) or defined(__clang__)
         return details::parse_mangling<details::type_prefix_tag>(__PRETTY_FUNCTION__, __FUNCTION__);
@@ -75,17 +50,9 @@ namespace csl::typeinfo
         static_assert(false, "csl::typeinfo : unhandled plateform");
 #endif
     }
-    template <typename T>
-    constexpr inline static auto type_name_v = type_name<T>();
 
     template <auto value>
-    [[nodiscard]] constexpr static auto type_name(/*no parameters allowed*/) -> std::string_view
-    {
-        return type_name<decltype(value)>();
-    }
-
-    template <auto value>
-    [[nodiscard]] constexpr static auto value_name(/*no parameters allowed*/) -> std::string_view
+    [[nodiscard]] constexpr static auto make_value_name(/*no parameters allowed*/) -> std::string_view
     {
 #if defined(__GNUC__) or defined(__clang__)
         return details::parse_mangling<details::value_prefix_tag>(__PRETTY_FUNCTION__, __FUNCTION__);
@@ -95,8 +62,46 @@ namespace csl::typeinfo
         static_assert(false, "csl::typeinfo : unhandled plateform");
 #endif
     }
-    template <auto value>
-    constexpr inline static auto value_name_v = value_name<value>();
+}
+
+/// \brief constexpr typeinfo that does not relies on __cpp_rtti
+/// WARNIN: Produced outputs ARE NOT portable: inconsistencies exist across compilers (GCC, Clang, msvc-cl)
+///
+/// Known limitations :
+///
+///  [type_name]
+///     decl namespace (demo: https://godbolt.org/z/vhr4h4j9Y)
+///            GCC:    namespace is part of the type (ex: A::B::C::type_as_ns<>::func<>(int)::my_type)
+///            clang:  missing (ex: my_type)
+///     type aliases (demo: https://godbolt.org/z/9PEq9zWTn)
+///        ex : std::string
+///            std::basic_string<char>
+///            std::__cxx11::basic_string<char>
+///            std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char>>
+///     MSVC: "class "/"struct " prefixes stripped at the top level only
+///
+///  [value_name]
+///     values representation (demo: https://godbolt.org/z/bn9ofo3Pz)
+///      ex : int{42}
+///          GCC, Clang: "42"sv
+///          MSVC:       "0x2a"sv
+///      use <charconv> std::to_chars into std::string_view for reliable basic numerical values
+namespace csl::typeinfo
+{
+    template <typename T>
+    struct type_name {
+        constexpr static auto value = details::make_type_name<T>();
+    };
+    template <typename T>
+    constexpr inline static auto type_name_v = type_name<T>::value;
+
+
+    template <auto v>
+    struct value_name {
+        constexpr static auto value = details::make_value_name<v>();
+    };
+    template <auto v>
+    constexpr inline static auto value_name_v = value_name<v>::value;
 
     // TODO(Guss): hash_code, see #93 https://github.com/GuillaumeDua/CppShelf/issues/93
     //  - update example accordingly
